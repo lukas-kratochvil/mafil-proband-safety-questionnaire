47b4a68d470102083703ec5afb13e17d54b95d0c
Lukáš Kratochvíl <xkrato10@fi.muni.cz>
2022-07-18 22:59:55 +0200

    feat: added app theme - changed default background color

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,23 @@
+import CssBaseline from '@mui/material/CssBaseline';
+import createTheme from '@mui/material/styles/createTheme';
+import ThemeProvider from '@mui/material/styles/ThemeProvider';
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './App'
 
+const theme = createTheme({
+  palette: {
+    background: {
+      default: "rgb(248, 248, 248)",
+    },
+  },
+});
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline enableColorScheme />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
-)
+);
