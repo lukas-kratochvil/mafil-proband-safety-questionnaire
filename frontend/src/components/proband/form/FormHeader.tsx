a7656d73f339ff81a1d9e9b0f37e92a7febf6e48
Lukáš Kratochvíl <xkrato10@fi.muni.cz>
2022-07-22 09:57:20 +0200

    fix: removed unnecessary Grid component from the FormHeader

--- frontend/src/components/proband/form/FormHeader.tsx
+++ frontend/src/components/proband/form/FormHeader.tsx
@@ -32,36 +32,34 @@ const ContentCell = ({ title, value }: IContentCellProps) => {
 export const FormHeader = () => {
   return (
     <FormCard mainGridPadding={0}>
-      <Grid container>
-        <Grid
-          item
-          xs={10}
-          borderBottom={1}
-          borderRight={1}
-        >
-          <ContentCell title='Projekt' value='Jméno projektu' />
-        </Grid>
-        <Grid
-          item
-          xs={2}
-          borderBottom={1}
-        >
-          <ContentCell title='Fantom' value='N' />
-        </Grid>
+      <Grid
+        item
+        xs={10}
+        borderBottom={1}
+        borderRight={1}
+      >
+        <ContentCell title='Projekt' value='Jméno projektu' />
+      </Grid>
+      <Grid
+        item
+        xs={2}
+        borderBottom={1}
+      >
+        <ContentCell title='Fantom' value='N' />
+      </Grid>
 
-        <Grid
-          item
-          xs={7}
-          borderRight={1}
-        >
-          <ContentCell title='Přístroj' value='Magnet 1' />
-        </Grid>
-        <Grid
-          item
-          xs={5}
-        >
-          <ContentCell title='Datum měření' value={new Date().toDateString()} />
-        </Grid>
+      <Grid
+        item
+        xs={7}
+        borderRight={1}
+      >
+        <ContentCell title='Přístroj' value='Magnet 1' />
+      </Grid>
+      <Grid
+        item
+        xs={5}
+      >
+        <ContentCell title='Datum měření' value={new Date().toDateString()} />
       </Grid>
     </FormCard>
   );
