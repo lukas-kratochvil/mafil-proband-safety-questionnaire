408ec903378163f691abc57786e012a26d39fee7
Lukáš Kratochvíl <xkrato10@fi.muni.cz>
2022-07-23 15:43:37 +0200

    feat: deleted proband WelcomePage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,10 @@
 import { Route, Routes } from "react-router-dom";
-import { Navigation } from "./components/operator/navigation/Navigation";
 import { FormTemplate } from "./components/proband/form/FormTemplate";
-import { WelcomePage } from "./components/proband/welcomePage/WelcomePage";
 
 export const App = () => {
   return (
     <Routes>
-      {/* <Navigation /> */}
-      <Route path='/' element={<WelcomePage />} />
-      <Route path='/form' element={<FormTemplate />} />
+      <Route path='/' element={<FormTemplate />} />
     </Routes>
   );
 };
