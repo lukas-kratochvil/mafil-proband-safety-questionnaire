4515ab9cd77f7a160349c6b20cd1513cd65b4cbb
Lukáš Kratochvíl <xkrato10@fi.muni.cz>
2022-08-22 22:26:22 +0200

    feat: refactoring - moved navigation tabs panel data inside the navigation component

--- frontend/src/components/navigation/Navigation.tsx
+++ frontend/src/components/navigation/Navigation.tsx
@@ -1,37 +1,43 @@
 import { Box, Tab, Tabs } from "@mui/material";
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { tabs } from "../../data/navigation_data";
 
 export interface ILinkTabProps {
   label: string;
-  link: string;
+  onClick: () => void;
 }
 
-const LinkTab = ({ label, link }: ILinkTabProps) => {
-  const navigate = useNavigate();
-
-  const handleOnClickTab = () => navigate(link);
-
-  return (
-    <Tab
-      component="a"
-      label={label}
-      onClick={handleOnClickTab}
-      sx={{
-        height: "3rem",
-        borderRight: 1,
-        borderColor: "black",
-        "&:hover": {
-          backgroundColor: "#ffc04d",
-        },
-      }}
-    />
-  );
-};
+const LinkTab = ({ label, onClick }: ILinkTabProps) => (
+  <Tab
+    component="a"
+    label={label}
+    onClick={onClick}
+    sx={{
+      height: "3rem",
+      borderRight: 1,
+      borderColor: "black",
+      "&:hover": {
+        backgroundColor: "#ffc04d",
+      },
+    }}
+  />
+);
 
 export const Navigation = () => {
+  const navigate = useNavigate();
   const [value, setValue] = useState<number>(0);
+
+  const tabs: ILinkTabProps[] = [
+    {
+      // TODO: number must be updated
+      label: "Čekárna (2)",
+      onClick: () => navigate("/auth/waiting-room"),
+    },
+    {
+      label: "Poslední visity",
+      onClick: () => navigate("/auth/recent-visits"),
+    },
+  ];
 
   const handleChange = (event: React.SyntheticEvent, newValue: number) => setValue(newValue);
 
