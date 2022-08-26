de3b52e089f644feca0f45e1cf7af9161997a775
Lukáš Kratochvíl <xkrato10@fi.muni.cz>
2022-08-26 18:36:13 +0200

    feat: added czech localization for date pickers

--- frontend/src/components/form/inputs/FormDatePicker.tsx
+++ frontend/src/components/form/inputs/FormDatePicker.tsx
@@ -1,6 +1,7 @@
 import { TextField } from "@mui/material";
 import { DesktopDatePicker, LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
+import CsLocale from "date-fns/locale/cs";
 import { Controller } from "react-hook-form";
 import { IFormDefaultInputProps } from "./form_input";
 
@@ -8,7 +9,15 @@ export const FormDatePicker = ({ name, label, disabled }: IFormDefaultInputProps
   <Controller
     name={name}
     render={({ field }) => (
-      <LocalizationProvider dateAdapter={AdapterDateFns}>
+      <LocalizationProvider
+        dateAdapter={AdapterDateFns}
+        /**
+         * TODO:
+         *  - change when different language is chosen
+         *  - find 'Answer : 6' on this page https://www.anycodings.com/2022/01/reactjs-material-ui-how-to-use.html and create similar language mapping
+         */
+        adapterLocale={CsLocale}
+      >
         <DesktopDatePicker
           {...field}
           label={label}
