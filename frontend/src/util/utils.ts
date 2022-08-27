00dd7bcf5f6485b5d2b63fd56582580303712386
Lukáš Kratochvíl <xkrato10@fi.muni.cz>
2022-08-27 10:02:41 +0200

    feat: created questions fetching functions

--- frontend/src/util/utils.ts
+++ frontend/src/util/utils.ts
@@ -1,3 +1,4 @@
+import { IQuestionData, questions1, questions2 } from "../data/form_data";
 import { dummyVisits, IProbandVisit, VisitState } from "../data/visit_data";
 
 export const fetchVisit = (visitId: string): IProbandVisit | undefined =>
@@ -11,3 +12,14 @@ export const fetchWaitingRoomVisits = (): IProbandVisit[] =>
 export const fetchRecentVisits = (): IProbandVisit[] =>
   // TODO: get visits from DB
   dummyVisits.filter((visit) => [VisitState.CHECKED, VisitState.SIGNED, VisitState.FANTOM_DONE].includes(visit.state));
+
+// TODO: get questions from DB
+export const fetchCurrentQuestionsPart1 = (): IQuestionData[] => questions1.filter((question) => question.isValid);
+
+// TODO: get questions from DB
+export const fetchCurrentQuestionsPart2 = (): IQuestionData[] => questions2.filter((question) => question.isValid);
+
+// TODO: get question from DB
+export const fetchQuestion = (questionId: string): IQuestionData | undefined =>
+  questions1.find((question) => question.id === questionId) ??
+  questions2.find((question) => question.id === questionId);
