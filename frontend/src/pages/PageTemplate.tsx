b603254be4c1f002ba7d1fe2f2110d76a8c0cdea
Lukáš Kratochvíl <lkratochvil57@gmail.com>
2022-09-17 23:41:10 +0200

    feat: added <main> element to contain page-specific content

--- frontend/src/pages/PageTemplate.tsx
+++ frontend/src/pages/PageTemplate.tsx
@@ -14,21 +14,23 @@ export const PageTemplate = ({ children, centerize, isTablePage }: PropsWithChil
   return (
     <>
       <Header />
-      <Stack
-        spacing={3}
-        sx={{
-          // content height is minus 4rem header and minus 3rem navigation (if viewed in the auth version)
-          height: centerize ? `calc(100vh - 4rem ${username === undefined ? "" : "- 3rem"})` : undefined,
-          width: "100%",
-          maxWidth: isTablePage ? "80%" : "67.5rem",
-          marginX: "auto",
-          marginY: centerize ? undefined : "3rem",
-          justifyContent: centerize ? "center" : undefined,
-          alignItems: centerize ? "center" : undefined,
-        }}
-      >
-        {children}
-      </Stack>
+      <main>
+        <Stack
+          spacing={3}
+          sx={{
+            // content height is minus 4rem header and minus 3rem navigation (if viewed in the auth version)
+            height: centerize ? `calc(100vh - 4rem ${username === undefined ? "" : "- 3rem"})` : undefined,
+            width: "100%",
+            maxWidth: isTablePage ? "80%" : "67.5rem",
+            marginX: "auto",
+            marginY: centerize ? undefined : "3rem",
+            justifyContent: centerize ? "center" : undefined,
+            alignItems: centerize ? "center" : undefined,
+          }}
+        >
+          {children}
+        </Stack>
+      </main>
     </>
   );
 };
