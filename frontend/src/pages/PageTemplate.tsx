2e339a600377dd14f2a17f3c2177eb231d282e0c
Lukáš Kratochvíl <lkratochvil57@gmail.com>
2022-09-25 18:18:31 +0200

    feat: table page content occupies 95% of the width

--- frontend/src/pages/PageTemplate.tsx
+++ frontend/src/pages/PageTemplate.tsx
@@ -21,9 +21,9 @@ export const PageTemplate = ({ children, centerize, isTablePage }: PropsWithChil
             // content height is minus 4rem header and minus 3rem navigation (if viewed in the auth version)
             height: centerize ? `calc(100vh - 4rem ${username === undefined ? "" : "- 3rem"})` : undefined,
             width: "100%",
-            maxWidth: isTablePage ? "90%" : "67.5rem",
+            maxWidth: isTablePage ? "95%" : "67.5rem",
             marginX: "auto",
-            marginY: centerize ? undefined : "3rem",
+            marginY: centerize ? "auto" : "3rem",
             justifyContent: centerize ? "center" : undefined,
             alignItems: centerize ? "center" : undefined,
           }}
