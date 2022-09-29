62c91ac441969f48bbc9c2d0876a6274644ca0d9
Lukáš Kratochvíl <lkratochvil57@gmail.com>
2022-09-29 12:12:09 +0200

    feat: edited CardBox border color and width

--- frontend/src/components/card/CardBox.tsx
+++ frontend/src/components/card/CardBox.tsx
@@ -1,28 +1,33 @@
-import { Card, Divider, Typography } from "@mui/material";
+import { Card, Divider, Typography, useTheme } from "@mui/material";
 import { PropsWithChildren } from "react";
 
 interface ICardBoxProps {
-  width?: string;
   title: string;
+  width?: string;
 }
 
-export const CardBox = ({ children, width = undefined, title }: PropsWithChildren<ICardBoxProps>) => (
-  <Card
-    sx={{
-      border: 2,
-      width,
-    }}
-  >
-    <Typography
-      textAlign="center"
-      fontWeight="bold"
-      fontSize="1.25rem"
-      padding="0.5rem"
-      bgcolor="#f0f4c3"
+export const CardBox = ({ children, title, width }: PropsWithChildren<ICardBoxProps>) => {
+  const theme = useTheme();
+
+  return (
+    <Card
+      sx={{
+        border: 1,
+        borderColor: theme.palette.grey[600],
+        width,
+      }}
     >
-      {title}
-    </Typography>
-    <Divider flexItem />
-    {children}
-  </Card>
-);
+      <Typography
+        textAlign="center"
+        fontWeight="bold"
+        fontSize="1.25rem"
+        padding="0.5rem"
+        bgcolor="#f0f4c3"
+      >
+        {title}
+      </Typography>
+      <Divider flexItem />
+      {children}
+    </Card>
+  );
+};
