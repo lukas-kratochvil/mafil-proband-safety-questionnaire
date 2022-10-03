78d7a6d99842a45fdbd352d1ce3cabfca0024dd0
Lukáš Kratochvíl <lkratochvil57@gmail.com>
2022-10-03 20:11:08 +0200

    feat: unauthorized user cannot see URIs for authorized users

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,5 @@
 import { Route, Routes } from "react-router-dom";
+import { useAuth } from "./hooks/auth/Auth";
 import { FormAfterSubmission } from "./pages/FormAfterSubmission";
 import { FormPage } from "./pages/FormPage";
 import { LoginPage } from "./pages/LoginPage";
@@ -6,35 +7,43 @@ import { RecentVisitsTablePage } from "./pages/RecentVisitsTablePage";
 import { VisitDetailPage } from "./pages/VisitDetailPage";
 import { WaitingRoomTablePage } from "./pages/WaitingRoomTablePage";
 
-export const App = () => (
-  <Routes>
-    <Route
-      path="/"
-      element={<FormPage />}
-    />
-    <Route
-      path="/form-after-submission"
-      element={<FormAfterSubmission />}
-    />
-    <Route
-      path="/auth"
-      element={<LoginPage />}
-    />
-    <Route
-      path="/auth/form/:id"
-      element={<FormPage />}
-    />
-    <Route
-      path="/auth/waiting-room"
-      element={<WaitingRoomTablePage />}
-    />
-    <Route
-      path="/auth/recent-visits"
-      element={<RecentVisitsTablePage />}
-    />
-    <Route
-      path="/auth/visit-detail/:id"
-      element={<VisitDetailPage />}
-    />
-  </Routes>
-);
+export const App = () => {
+  const { username } = useAuth();
+
+  return (
+    <Routes>
+      <Route
+        path="/"
+        element={<FormPage />}
+      />
+      <Route
+        path="/form-after-submission"
+        element={<FormAfterSubmission />}
+      />
+      <Route
+        path="/auth"
+        element={<LoginPage />}
+      />
+      {username && (
+        <>
+          <Route
+            path="/auth/form/:id"
+            element={<FormPage />}
+          />
+          <Route
+            path="/auth/waiting-room"
+            element={<WaitingRoomTablePage />}
+          />
+          <Route
+            path="/auth/recent-visits"
+            element={<RecentVisitsTablePage />}
+          />
+          <Route
+            path="/auth/visit-detail/:id"
+            element={<VisitDetailPage />}
+          />
+        </>
+      )}
+    </Routes>
+  );
+};
