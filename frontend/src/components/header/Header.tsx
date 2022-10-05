0c6723be8e5806f03dfbb6412bbb9069cd83a3c5
Lukáš Kratochvíl <lkratochvil57@gmail.com>
2022-10-05 22:47:14 +0200

    feat: renamed the navigation tab for ApprovalTable

--- frontend/src/components/header/Header.tsx
+++ frontend/src/components/header/Header.tsx
@@ -35,7 +35,7 @@ export const Header = () => {
       icon: <PendingActionsIcon />,
     },
     {
-      label: "Schvalování",
+      label: "Ke schválení (?)",
       onClick: () => navigate("/auth/approval"),
       icon: <ApprovalIcon />,
     },
