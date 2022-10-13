4328953654a38ec12f7ada839715f2403f7a7024
Lukáš Kratochvíl <lkratochvil57@gmail.com>
2022-10-13 23:04:38 +0200

    feat: edited eslint import rules

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,17 +4,17 @@ import { operatorFormSchema } from "./components/form/schemas/form-schema_operat
 import { loadFantomFormDefaultValues } from "./components/form/util/utils";
 import { useAuth } from "./hooks/auth/Auth";
 import { ApprovalTablePage } from "./pages/ApprovalTablePage";
+import { HomePage } from "./pages/HomePage";
+import { LoginPage } from "./pages/LoginPage";
+import { RecentVisitsTablePage } from "./pages/RecentVisitsTablePage";
+import { VisitDetailPage } from "./pages/VisitDetailPage";
+import { WaitingRoomTablePage } from "./pages/WaitingRoomTablePage";
 import { ApprovalFormPage } from "./pages/form/ApprovalFormPage";
 import { DuplicationFormPage } from "./pages/form/DuplicationFormPage";
 import { FantomFormPage } from "./pages/form/FantomFormPage";
 import { FormPageTemplate } from "./pages/form/FormPageTemplate";
 import { ProbandFormPage } from "./pages/form/ProbandFormPage";
 import { WaitingRoomFormPage } from "./pages/form/WaitingRoomFormPage";
-import { HomePage } from "./pages/HomePage";
-import { LoginPage } from "./pages/LoginPage";
-import { RecentVisitsTablePage } from "./pages/RecentVisitsTablePage";
-import { VisitDetailPage } from "./pages/VisitDetailPage";
-import { WaitingRoomTablePage } from "./pages/WaitingRoomTablePage";
 
 export const App = () => {
   const { operator } = useAuth();
