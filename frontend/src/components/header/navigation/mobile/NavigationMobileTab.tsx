76880d13a2b2d8fc6d002084fa79c4f55a389862
Lukáš Kratochvíl <lkratochvil57@gmail.com>
2022-10-14 23:41:35 +0200

    feat: used mechanism for highlighting the selected mobile navigation tab

--- frontend/src/components/header/navigation/mobile/NavigationMobileTab.tsx
+++ frontend/src/components/header/navigation/mobile/NavigationMobileTab.tsx
@@ -1,42 +1,51 @@
 import { Grid, ListItem, ListItemButton } from "@mui/material";
 import { amber } from "@mui/material/colors";
+import { useLocation } from "react-router-dom";
 import { INavigationItem } from "../../Header";
 
 interface INavigationMobileTabProps {
   tab: INavigationItem
 }
 
-export const NavigationMobileTab = ({ tab }: INavigationMobileTabProps) => (
-  <ListItem disablePadding>
-    <ListItemButton
-      onClick={tab.onClick}
-      sx={{
-        paddingY: "1rem",
-        color: "rgba(0, 0, 0, 1)",
-        opacity: 0.85,
-        fontSize: "0.85rem",
-        fontWeight: 500,
-        letterSpacing: "0.02857rem",
-        textTransform: "uppercase",
-        "&:hover": {
-          backgroundColor: amber[600],
-          opacity: 1,
-        },
-      }}
-    >
-      <Grid
-        container
-        alignItems="center"
-        columnGap="1rem"
+export const NavigationMobileTab = ({ tab }: INavigationMobileTabProps) => {
+  const location = useLocation();
+
+  const isSelected = location.pathname.startsWith(tab.urlPrefix);
+  const highlightColor = amber[600];
+
+  return (
+    <ListItem disablePadding>
+      <ListItemButton
+        onClick={tab.onClick}
+        sx={{
+          paddingY: "1rem",
+          color: "rgba(0, 0, 0, 1)",
+          backgroundColor: isSelected ? highlightColor : undefined,
+          opacity: 0.85,
+          fontSize: "0.85rem",
+          fontWeight: 500,
+          letterSpacing: "0.02857rem",
+          textTransform: "uppercase",
+          "&:hover": {
+            backgroundColor: highlightColor,
+            opacity: 1,
+          },
+        }}
       >
-        <Grid item>{tab.icon}</Grid>
         <Grid
-          item
-          xs
+          container
+          alignItems="center"
+          columnGap="1rem"
         >
-          {tab.label}
+          <Grid item>{tab.icon}</Grid>
+          <Grid
+            item
+            xs
+          >
+            {tab.label}
+          </Grid>
         </Grid>
-      </Grid>
-    </ListItemButton>
-  </ListItem>
-);
+      </ListItemButton>
+    </ListItem>
+  );
+};
