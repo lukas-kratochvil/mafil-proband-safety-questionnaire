151b2c5382e2bb84bac7619d7b657802840220ea
Lukáš Kratochvíl <lkratochvil57@gmail.com>
2022-10-15 22:55:22 +0200

    feat: init i18n localization

---
+++ frontend/src/i18n.ts
@@ -0,0 +1,22 @@
+import i18n from "i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import { initReactI18next } from "react-i18next";
+
+const resources = {};
+
+export const availableLanguages = Object.keys(resources);
+
+i18n
+  // detect user language
+  .use(LanguageDetector)
+  // pass the i18n instance to react-i18next
+  .use(initReactI18next)
+  // init i18next
+  .init({
+    debug: true,
+    fallbackLng: "cz",
+    interpolation: {
+      escapeValue: false, // not needed for react as it escapes by default
+    },
+    resources,
+  });
