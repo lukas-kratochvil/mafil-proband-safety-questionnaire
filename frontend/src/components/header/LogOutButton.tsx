925def8b8619871865e07330f4845664fe70ab31
Lukáš Kratochvíl <lkratochvil57@gmail.com>
2022-10-16 23:36:22 +0200

    feat: localized LogOutButton

--- frontend/src/components/header/LogOutButton.tsx
+++ frontend/src/components/header/LogOutButton.tsx
@@ -1,8 +1,11 @@
 import { Button } from "@mui/material";
 import { red } from "@mui/material/colors";
+import { useTranslation } from "react-i18next";
 import { useAuth } from "../../hooks/auth/Auth";
+import { defaultNS } from "../../i18n";
 
 export const LogOutButton = () => {
+  const { t } = useTranslation(defaultNS, { keyPrefix: "common" });
   const { logOut } = useAuth();
 
   return (
@@ -18,7 +21,7 @@ export const LogOutButton = () => {
         },
       }}
     >
-      Odhlásit se
+      {t("logOutButton")}
     </Button>
   );
 };
