36b552439cb8f867273c83a69eccc31b47c69e03
Lukáš Kratochvíl <lkratochvil57@gmail.com>
2022-10-16 20:20:58 +0200

    feat: added i18n support for the Typescript and initialized CZ, EN and SK localization files

--- frontend/src/i18n.ts
+++ frontend/src/i18n.ts
@@ -1,12 +1,20 @@
-import i18n from "i18next";
+import i18next from "i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
+import cz from "./i18n/cz.json";
+import en from "./i18n/en.json";
+import sk from "./i18n/sk.json";
 
-const resources = {};
+export const defaultNS = "translation";
+export const resources = {
+  cz,
+  en,
+  sk,
+} as const;
 
 export const availableLanguages = Object.keys(resources);
 
-i18n
+i18next
   // detect user language
   .use(LanguageDetector)
   // pass the i18n instance to react-i18next
@@ -14,9 +22,11 @@ i18n
   // init i18next
   .init({
     debug: process.env.NODE_ENV === "development",
+    defaultNS,
     fallbackLng: "cz",
     interpolation: {
-      escapeValue: false, // not needed for react as it escapes by default
+      escapeValue: false, // not needed for React as it escapes by default
     },
+    // TODO: try to get {locale}.json files from the backend (https://www.i18next.com/overview/configuration-options#languages-namespaces-resources)
     resources,
   });
