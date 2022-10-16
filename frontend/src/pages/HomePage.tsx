517996a12e9dd8541b889ede3f5c24757ed45f11
Lukáš Kratochvíl <lkratochvil57@gmail.com>
2022-10-16 22:29:25 +0200

    feat: localized HomePage

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,15 +1,18 @@
 import { Button, Grid } from "@mui/material";
+import { useTranslation } from "react-i18next";
 import { useNavigate } from "react-router-dom";
 import { UrlBasePaths } from "../App";
 import { CardContainer } from "../components/card/CardContainer";
+import { defaultNS } from "../i18n";
 import { PageContainer } from "./PageContainer";
 
 export const HomePage = () => {
+  const { t } = useTranslation(defaultNS, { keyPrefix: "homePage" });
   const navigate = useNavigate();
 
   return (
     <PageContainer centerize>
-      <CardContainer title="Váš formulář byl úspěšně odeslán. Vyčkejte, prosím, na další pokyny operátora.">
+      <CardContainer title={t("title")}>
         <Grid
           container
           justifyContent="center"
@@ -20,7 +23,7 @@ export const HomePage = () => {
             variant="contained"
             onClick={() => navigate(UrlBasePaths.PROBAND_FORM)}
           >
-            Otevřít nový formulář
+            {t("openNewFormButton")}
           </Button>
         </Grid>
       </CardContainer>
