9c2142fa90d000daca6e0c7cc87bb0379c024fb0
Lukáš Kratochvíl <lkratochvil57@gmail.com>
2022-10-17 19:42:04 +0200

    feat: localized FormProbandContact

--- frontend/src/components/form/FormProbandContact.tsx
+++ frontend/src/components/form/FormProbandContact.tsx
@@ -1,52 +1,56 @@
 import { Grid, Typography } from "@mui/material";
+import { useTranslation } from "react-i18next";
+import { defaultNS } from "../../i18n";
 import { ErrorFeedback } from "./ErrorFeedback";
 import { FormCardContainer } from "./FormCardContainer";
 import { FormTextField } from "./inputs/FormTextField";
 import { IFormInputsProps } from "./types/types";
 
-export const FormProbandContact = ({ disableInputs }: IFormInputsProps) => (
-  <FormCardContainer title="Kontaktní údaje">
-    <Grid
-      container
-      direction="row"
-      spacing="1rem"
-      columns={2}
-    >
+export const FormProbandContact = ({ disableInputs }: IFormInputsProps) => {
+  const { t } = useTranslation(defaultNS, { keyPrefix: "form.probandContact" });
+
+  return (
+    <FormCardContainer title={t("title")}>
       <Grid
-        item
-        xs={2}
+        container
+        direction="row"
+        spacing="1rem"
+        columns={2}
       >
-        <Typography>
-          V případě, že budete chtít předat Vaše naměřená data elektronickou cestou, vyplňte údaje níže:
-        </Typography>
+        <Grid
+          item
+          xs={2}
+        >
+          <Typography>{t("text")}</Typography>
+        </Grid>
+        <Grid
+          item
+          xs={2}
+          sm={1}
+        >
+          <FormTextField
+            name="email"
+            label={t("email")}
+            isOptional
+            disabled={disableInputs}
+          />
+          <ErrorFeedback name="email" />
+        </Grid>
+        <Grid
+          item
+          xs={2}
+          sm={1}
+        >
+          <FormTextField
+            name="phoneNumber"
+            label={t("phone")}
+            type="tel"
+            isOptional
+            disabled={disableInputs}
+          />
+          <ErrorFeedback name="phoneNumber" />
+        </Grid>
       </Grid>
-      <Grid
-        item
-        xs={2}
-        sm={1}
-      >
-        <FormTextField
-          name="email"
-          label="Email"
-          isOptional
-          disabled={disableInputs}
-        />
-        <ErrorFeedback name="email" />
-      </Grid>
-      <Grid
-        item
-        xs={2}
-        sm={1}
-      >
-        <FormTextField
-          name="phoneNumber"
-          label="Telefonní číslo"
-          type="tel"
-          isOptional
-          disabled={disableInputs}
-        />
-        <ErrorFeedback name="phoneNumber" />
-      </Grid>
-    </Grid>
-  </FormCardContainer>
-);
+    </FormCardContainer>
+  );
+};
