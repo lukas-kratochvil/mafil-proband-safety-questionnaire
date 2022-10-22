3448ede2ae3b2aee204bfa8b9d9abcbeebc8e395
Lukáš Kratochvíl <lkratochvil57@gmail.com>
2022-10-22 11:46:58 +0200

    feat: localized ApprovalRoomTablePage

--- frontend/src/pages/ApprovalTablePage.tsx
+++ frontend/src/pages/ApprovalTablePage.tsx
@@ -1,60 +1,16 @@
 import { Button } from "@mui/material";
 import { compareAsc, format, parse } from "date-fns";
 import { MRT_ColumnDef as MRTColumnDef } from "material-react-table";
+import { useTranslation } from "react-i18next";
 import { useNavigate } from "react-router-dom";
 import { UrlBasePaths } from "../App";
 import { IActionButtonsProps, InteractingTable } from "../components/table/InteractingTable";
 import { IVisit } from "../data/visit_data";
+import { defaultNS } from "../i18n";
 import { fetchApprovalVisits } from "../util/fetch";
 
-const createdAtFormat = "d.M.yyyy H:mm";
-const probandBirthdateFormat = "d.M.yyyy";
-
-const header: MRTColumnDef<IVisit>[] = [
-  {
-    accessorFn: (visit) => format(visit.createdAt, createdAtFormat),
-    id: "createdAt",
-    header: "Datum registrace",
-    sortingFn: (rowA, rowB, columnId) =>
-      compareAsc(
-        parse(`${rowA.getValue(columnId)}`, createdAtFormat, new Date()),
-        parse(`${rowB.getValue(columnId)}`, createdAtFormat, new Date())
-      ),
-  },
-  {
-    accessorKey: "projectInfo.project",
-    header: "Projekt",
-  },
-  {
-    accessorFn: (visit) => `${visit.probandInfo.surname}, ${visit.probandInfo.name}`,
-    id: "proband",
-    header: "Proband",
-  },
-  {
-    accessorKey: "probandInfo.personalId",
-    header: "Rodné číslo",
-  },
-  {
-    accessorFn: (visit) => format(visit.probandInfo.birthdate, probandBirthdateFormat),
-    id: "probandInfo.birthdate",
-    header: "Datum narození",
-    sortingFn: (rowA, rowB, columnId) =>
-      compareAsc(
-        parse(`${rowA.getValue(columnId)}`, probandBirthdateFormat, new Date()),
-        parse(`${rowB.getValue(columnId)}`, probandBirthdateFormat, new Date())
-      ),
-  },
-  {
-    accessorKey: "probandInfo.gender",
-    header: "Pohlaví",
-  },
-  {
-    accessorKey: "probandInfo.nativeLanguage",
-    header: "Mateřský jazyk",
-  },
-];
-
 const ApprovalActionButtons = ({ visitId }: IActionButtonsProps) => {
+  const { t } = useTranslation(defaultNS, { keyPrefix: "approvalRoomTablePage.actions" });
   const navigate = useNavigate();
 
   return (
@@ -63,16 +19,67 @@ const ApprovalActionButtons = ({ visitId }: IActionButtonsProps) => {
       variant="contained"
       onClick={() => navigate(`${UrlBasePaths.APPROVAL}/form/${visitId}`)}
     >
-      Zobrazit
+      {t("showButton")}
     </Button>
   );
 };
 
-export const ApprovalTablePage = () => (
-  <InteractingTable
-    header={header}
-    fetchVisits={fetchApprovalVisits}
-    ActionButtons={ApprovalActionButtons}
-    actionButtonsSize={100}
-  />
-);
+const createdAtFormat = "d.M.yyyy H:mm";
+const probandBirthdateFormat = "d.M.yyyy";
+
+export const ApprovalTablePage = () => {
+  const { t } = useTranslation(defaultNS, { keyPrefix: "approvalRoomTablePage.header" });
+
+  const header: MRTColumnDef<IVisit>[] = [
+    {
+      accessorFn: (visit) => format(visit.createdAt, createdAtFormat),
+      id: "createdAt",
+      header: t("registrationDate"),
+      sortingFn: (rowA, rowB, columnId) =>
+        compareAsc(
+          parse(`${rowA.getValue(columnId)}`, createdAtFormat, new Date()),
+          parse(`${rowB.getValue(columnId)}`, createdAtFormat, new Date())
+        ),
+    },
+    {
+      accessorKey: "projectInfo.project",
+      header: t("project"),
+    },
+    {
+      accessorFn: (visit) => `${visit.probandInfo.surname}, ${visit.probandInfo.name}`,
+      id: "proband",
+      header: t("proband"),
+    },
+    {
+      accessorKey: "probandInfo.personalId",
+      header: t("personalId"),
+    },
+    {
+      accessorFn: (visit) => format(visit.probandInfo.birthdate, probandBirthdateFormat),
+      id: "probandInfo.birthdate",
+      header: t("birthdate"),
+      sortingFn: (rowA, rowB, columnId) =>
+        compareAsc(
+          parse(`${rowA.getValue(columnId)}`, probandBirthdateFormat, new Date()),
+          parse(`${rowB.getValue(columnId)}`, probandBirthdateFormat, new Date())
+        ),
+    },
+    {
+      accessorKey: "probandInfo.gender",
+      header: t("gender"),
+    },
+    {
+      accessorKey: "probandInfo.nativeLanguage",
+      header: t("nativeLanguage"),
+    },
+  ];
+
+  return (
+    <InteractingTable
+      header={header}
+      fetchVisits={fetchApprovalVisits}
+      ActionButtons={ApprovalActionButtons}
+      actionButtonsSize={100}
+    />
+  );
+};
