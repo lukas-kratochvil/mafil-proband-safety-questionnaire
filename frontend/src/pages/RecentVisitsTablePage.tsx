107b9e1783115177b45b79221a30ab4eef325550
Lukáš Kratochvíl <lkratochvil57@gmail.com>
2022-10-22 12:11:51 +0200

    feat: localized RecentVisitsTablePage

--- frontend/src/pages/RecentVisitsTablePage.tsx
+++ frontend/src/pages/RecentVisitsTablePage.tsx
@@ -1,58 +1,17 @@
 import { Button } from "@mui/material";
 import { compareAsc, format, parse } from "date-fns";
 import { MRT_ColumnDef as MRTColumnDef } from "material-react-table";
+import { useTranslation } from "react-i18next";
 import { useNavigate } from "react-router-dom";
 import { UrlBasePaths } from "../App";
 import { IActionButtonsProps, InteractingTable } from "../components/table/InteractingTable";
 import { IVisit } from "../data/visit_data";
+import { defaultNS } from "../i18n";
 import { fetchRecentVisits } from "../util/fetch";
 import { getDummyVisit } from "../util/fetch.dev";
 
-const processedDateFormat = "d.M.yyyy H:mm";
-
-const header: MRTColumnDef<IVisit>[] = [
-  {
-    accessorKey: "visitId",
-    header: "Visit ID",
-    size: 100,
-  },
-  {
-    accessorFn: (visit) => `${visit.probandInfo.surname}, ${visit.probandInfo.name}`,
-    id: "proband",
-    header: "Proband",
-  },
-  {
-    accessorKey: "projectInfo.project",
-    header: "Projekt",
-  },
-  {
-    accessorKey: "projectInfo.device",
-    header: "Přístroj",
-  },
-  {
-    // TODO: change 'visit.createdAt' for some other attribute - firstly, we need to solve which attribute should be created for this purpose
-    accessorFn: (visit) => format(visit.createdAt, processedDateFormat),
-    id: "processedDate",
-    header: "Zpracováno",
-    sortingFn: (rowA, rowB, columnId) =>
-      compareAsc(
-        parse(`${rowA.getValue(columnId)}`, processedDateFormat, new Date()),
-        parse(`${rowB.getValue(columnId)}`, processedDateFormat, new Date())
-      ),
-  },
-  {
-    accessorFn: () => "MUNI_operator",
-    id: "processUser",
-    header: "Zpracoval",
-  },
-  {
-    accessorFn: (visit) => (visit.projectInfo.isPhantom ? "Dokončeno" : visit.state),
-    id: "state",
-    header: "Stav",
-  },
-];
-
 const RecentVisitsActionButtons = ({ visitId }: IActionButtonsProps) => {
+  const { t } = useTranslation(defaultNS, { keyPrefix: "recentVisitsTablePage.actions" })
   const navigate = useNavigate();
 
   return (
@@ -62,7 +21,7 @@ const RecentVisitsActionButtons = ({ visitId }: IActionButtonsProps) => {
         variant="contained"
         onClick={() => navigate(`${UrlBasePaths.RECENT_VISITS}/visit/${visitId}`)}
       >
-        Zobrazit detail
+        {t("showDetailButton")}
       </Button>
       <Button
         size="small"
@@ -79,17 +38,65 @@ const RecentVisitsActionButtons = ({ visitId }: IActionButtonsProps) => {
           }
         }}
       >
-        Duplikovat
+        {t("duplicateButton")}
       </Button>
     </>
   );
 };
 
-export const RecentVisitsTablePage = () => (
-  <InteractingTable
-    header={header}
-    fetchVisits={fetchRecentVisits}
-    ActionButtons={RecentVisitsActionButtons}
-    actionButtonsSize={320}
-  />
-);
+const processedDateFormat = "d.M.yyyy H:mm";
+
+export const RecentVisitsTablePage = () => {
+  const { t } = useTranslation(defaultNS, { keyPrefix: "recentVisitsTablePage.header" })
+
+  const header: MRTColumnDef<IVisit>[] = [
+    {
+      accessorKey: "visitId",
+      header: "Visit ID",
+      size: 100,
+    },
+    {
+      accessorFn: (visit) => `${visit.probandInfo.surname}, ${visit.probandInfo.name}`,
+      id: "proband",
+      header: t("proband"),
+    },
+    {
+      accessorKey: "projectInfo.project",
+      header: t("project"),
+    },
+    {
+      accessorKey: "projectInfo.device",
+      header: t("device"),
+    },
+    {
+      // TODO: change 'visit.createdAt' for some other attribute - firstly, we need to solve which attribute should be created for this purpose
+      accessorFn: (visit) => format(visit.createdAt, processedDateFormat),
+      id: "processedDate",
+      header: t("processedDate"),
+      sortingFn: (rowA, rowB, columnId) =>
+        compareAsc(
+          parse(`${rowA.getValue(columnId)}`, processedDateFormat, new Date()),
+          parse(`${rowB.getValue(columnId)}`, processedDateFormat, new Date())
+        ),
+    },
+    {
+      accessorFn: () => "MUNI_operator",
+      id: "processUser",
+      header: t("operatorProcessed"),
+    },
+    {
+      accessorFn: (visit) => (visit.projectInfo.isPhantom ? "Dokončeno" : visit.state),
+      id: "state",
+      header: t("state"),
+    },
+  ];
+
+  return (
+    <InteractingTable
+      header={header}
+      fetchVisits={fetchRecentVisits}
+      ActionButtons={RecentVisitsActionButtons}
+      actionButtonsSize={320}
+    />
+  );
+}
