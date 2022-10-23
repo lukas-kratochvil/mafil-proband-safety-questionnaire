dec0e17cdaf7faa5e51adc1f30e4439191651ace
Lukáš Kratochvíl <lkratochvil57@gmail.com>
2022-10-23 22:10:28 +0200

    feat: corrected vitest configuration

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -10,7 +10,8 @@ import { defineConfig } from "vite";
 export default defineConfig({
   plugins: [react()],
   test: {
+    environment: "jsdom",
     globals: true,
-    setupFiles: ["./test/setup.ts"],
+    setupFiles: ["./src/tests-setup.ts"],
   },
 })
