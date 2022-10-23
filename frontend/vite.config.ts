3e0be88cb2fa3806e11eec1bee2fa37dd4309187
Lukáš Kratochvíl <lkratochvil57@gmail.com>
2022-10-23 17:24:35 +0200

    feat: frontend tests setup

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/// <reference types="vitest" />
 
-// https://vitejs.dev/config/
+// eslint-disable-next-line import/no-extraneous-dependencies
+import react from "@vitejs/plugin-react";
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { defineConfig } from "vite";
+
+// Vite: https://vitejs.dev/config/
+// Vitest: https://vitest.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  test: {
+    globals: true,
+    setupFiles: ["./test/setup.ts"],
+  },
 })
