212dcba1cd7744b8351b17091f39154f1bec5fc8
Lukáš Kratochvíl <lkratochvil57@gmail.com>
2022-11-06 23:28:11 +0100

    fix: moved __mocks__ inside the src/__tests__ folder

--- frontend/src/__tests__/__mocks__/MockedProviders.tsx
+++ frontend/src/__tests__/__mocks__/MockedProviders.tsx
@@ -1,7 +1,15 @@
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { PropsWithChildren } from "react";
+import { I18nextProvider } from "react-i18next";
+import i18n from "../../i18n";
+
 
 export const MockedProviders = ({ children }: PropsWithChildren) => {
   const theme = createTheme({});
-  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+
+  return (
+    <I18nextProvider i18n={i18n}>
+      <ThemeProvider theme={theme}>{children}</ThemeProvider>
+    </I18nextProvider>
+  );
 };
