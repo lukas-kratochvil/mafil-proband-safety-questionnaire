25113a185a2420a3d46a92c0d65433e12c3281e9
Lukáš Kratochvíl <lkratochvil57@gmail.com>
2022-11-08 00:14:27 +0100

    feat: created logout button tests

---
+++ frontend/src/components/header/__tests__/logout-button.test.tsx
@@ -0,0 +1,35 @@
+import userEvent from "@testing-library/user-event";
+import { render, screen } from "../../../__tests__/utils";
+import i18n from "../../../i18n";
+import { LogOutButton } from "../LogOutButton";
+
+const logOutSpy = vi.fn();
+
+vi.mock("../../../hooks/auth/auth", () => ({
+  useAuth: () => ({
+    logOut: logOutSpy,
+  }),
+}));
+
+describe("logout-button", () => {
+  beforeEach(async () => {
+    await i18n.changeLanguage("cimode");
+  });
+
+  test("logOut button has title", async () => {
+    render(<LogOutButton />);
+
+    const logOutButton = screen.getByRole("button");
+
+    expect(logOutButton).toHaveTextContent(/^common.logOutButton$/);
+  });
+
+  test("log out if logOut button is clicked", async () => {
+    render(<LogOutButton />);
+
+    const logOutButton = screen.getByRole("button");
+    await userEvent.click(logOutButton);
+
+    expect(logOutSpy).toHaveBeenCalledTimes(1);
+  });
+});
