4b4ea311e078196754c9fbb67687a2f6d220b36b
Lukáš Kratochvíl <lkratochvil57@gmail.com>
2022-11-08 23:06:16 +0100

    feat: created FormInputFieldContainer tests

---
+++ frontend/src/components/form/inputs/__tests__/form-input-field-container.test.tsx
@@ -0,0 +1,42 @@
+import { render } from "@test-utils";
+import i18n from "src/i18n";
+import { FormInputFieldContainer } from "../FormInputFieldContainer";
+
+vi.mock("../../ErrorFeedback", () => ({
+  ErrorFeedback: () => <></>,
+}));
+
+describe("form-input-field-container", () => {
+  beforeEach(() => {
+    i18n.changeLanguage("cimode");
+  });
+
+  test("required field contains only label", () => {
+    const label = "Label";
+
+    const { container } = render(
+      <FormInputFieldContainer
+        label={label}
+        name=""
+      />
+    );
+
+    expect(container).toHaveTextContent(new RegExp(`^${label}$`));
+    expect(container).not.toHaveTextContent("(form.common.optional)");
+  });
+
+  test("optional field contains label with 'optional' text", () => {
+    const label = "Label";
+
+    const { container } = render(
+      <FormInputFieldContainer
+        label={label}
+        name=""
+        isOptional
+      />
+    );
+
+    expect(container).toHaveTextContent(new RegExp(`^${label}`));
+    expect(container).toHaveTextContent(/\(form.common.optional\)$/);
+  });
+});
