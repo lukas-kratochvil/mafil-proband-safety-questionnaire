22d3cabb459544e2be437e60cfbf1bfcc70cb69c
Lukáš Kratochvíl <lkratochvil57@gmail.com>
2022-11-13 00:06:59 +0100

    feat: created form entry info tests

---
+++ frontend/src/components/form/__tests__/form-entry-info.test.tsx
@@ -0,0 +1,18 @@
+import { render } from "@test-utils";
+import i18n from "src/i18n";
+import { FormEntryInfo } from "../FormEntryInfo";
+
+
+describe("form entry info", () => {
+  beforeEach(async () => {
+    await i18n.changeLanguage("cimode");
+  });
+
+  test("has title and text", async () => {
+    const { container } = render(<FormEntryInfo />);
+
+    expect(container).toHaveTextContent(/^form.entryInfo.title/);
+    expect(container).toHaveTextContent(/form.entryInfo.text1/);
+    expect(container).toHaveTextContent(/form.entryInfo.text2$/);
+  });
+});
