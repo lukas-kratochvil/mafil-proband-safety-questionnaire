e3fbfe879a88a0663487b1fbb8058edc5ed82d5e
Lukáš Kratochvíl <lkratochvil57@gmail.com>
2022-11-13 00:05:19 +0100

    feat: created form before examination tests

---
+++ frontend/src/components/form/__tests__/form-before-examination.test.tsx
@@ -0,0 +1,17 @@
+import { render } from "@test-utils";
+import i18n from "src/i18n";
+import { FormBeforeExamination } from "../FormBeforeExamination";
+
+
+describe("form before examination", () => {
+  beforeEach(async () => {
+    await i18n.changeLanguage("cimode");
+  });
+
+  test("has title and text", async () => {
+    const { container } = render(<FormBeforeExamination />);
+
+    expect(container).toHaveTextContent(/^form.beforeExamination.title/);
+    expect(container).toHaveTextContent(/form.beforeExamination.text$/);
+  });
+});
