c302cb8c6fcd4c592fda6d66d4c7b83a51d71e8a
Lukáš Kratochvíl <lkratochvil57@gmail.com>
2022-11-13 18:27:27 +0100

    feat: created form proband contact tests

---
+++ frontend/src/components/form/__tests__/form-proband-contact.test.tsx
@@ -0,0 +1,36 @@
+import { render, screen } from "@test-utils";
+import i18n from "src/i18n";
+import { FormProbandContact } from "../FormProbandContact";
+
+vi.mock("react-hook-form", () => ({
+  Controller: () => <div data-testid="input" />,
+}));
+
+vi.mock("../ErrorFeedback", () => ({
+  ErrorFeedback: () => <div />,
+}));
+
+describe("form proband contact", () => {
+  beforeEach(async () => {
+    await i18n.changeLanguage("cimode");
+  });
+
+  test("contains translations", () => {
+    const { container } = render(<FormProbandContact />);
+
+    expect(container).toHaveTextContent(/form.probandContact.title/);
+    expect(container).toHaveTextContent(/form.probandContact.text/);
+    expect(container).toHaveTextContent(/form.probandContact.email/);
+    expect(container).toHaveTextContent(/form.common.optional/);
+    expect(container).toHaveTextContent(/form.probandContact.phone/);
+    expect(container).toHaveTextContent(/form.common.optional/);
+  });
+
+  test("has all input fields", () => {
+    render(<FormProbandContact />);
+
+    const inputs = screen.queryAllByTestId("input");
+
+    expect(inputs.length).toEqual(2);
+  });
+});
