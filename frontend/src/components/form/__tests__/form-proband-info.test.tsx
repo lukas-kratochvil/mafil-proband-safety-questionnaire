1c31072e39468b673e0202672ff9564ba477ca1f
Lukáš Kratochvíl <lkratochvil57@gmail.com>
2022-11-13 21:38:54 +0100

    feat: created form proband info tests

---
+++ frontend/src/components/form/__tests__/form-proband-info.test.tsx
@@ -0,0 +1,52 @@
+import { render, screen } from "@test-utils";
+import i18n from "src/i18n";
+import { FormProbandInfo } from "../FormProbandInfo";
+
+vi.mock("react", () => ({
+  useEffect: vi.fn(),
+}));
+
+vi.mock("react-hook-form", () => ({
+  Controller: () => <div data-testid="input" />,
+  useFormContext: () => ({
+    getFieldState: vi.fn(),
+    resetField: vi.fn(),
+    setValue: vi.fn(),
+  }),
+  useWatch: vi.fn(),
+}));
+
+vi.mock("../ErrorFeedback", () => ({
+  ErrorFeedback: () => <div />,
+}));
+
+describe("form proband info", () => {
+  beforeEach(async () => {
+    await i18n.changeLanguage("cimode");
+  });
+
+  test("contains translations", () => {
+    const { container } = render(<FormProbandInfo />);
+
+    expect(container).toHaveTextContent(/form.probandInfo.title/);
+    expect(container).toHaveTextContent(/form.probandInfo.name/);
+    expect(container).toHaveTextContent(/form.probandInfo.surname/);
+    expect(container).toHaveTextContent(/form.probandInfo.personalId/);
+    expect(container).toHaveTextContent(/form.probandInfo.birthdate/);
+    expect(container).toHaveTextContent(/form.probandInfo.gender/);
+    expect(container).toHaveTextContent(/form.probandInfo.nativeLanguage/);
+    expect(container).toHaveTextContent(/form.probandInfo.height/);
+    expect(container).toHaveTextContent(/form.probandInfo.weight/);
+    expect(container).toHaveTextContent(/form.probandInfo.visualCorrection/);
+    expect(container).toHaveTextContent(/form.probandInfo.visualCorrectionValue/);
+    expect(container).toHaveTextContent(/form.probandInfo.sideDominance/);
+  });
+
+  test("has all input fields", () => {
+    render(<FormProbandInfo />);
+
+    const inputs = screen.getAllByTestId("input");
+
+    expect(inputs.length).toEqual(11);
+  });
+});
