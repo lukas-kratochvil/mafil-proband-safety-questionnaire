15026b6f9a84846b96a4a8de4bfefc6227b09a90
Lukáš Kratochvíl <lkratochvil57@gmail.com>
2022-11-14 15:12:22 +0100

    feat: created home page tests

---
+++ frontend/src/pages/__tests__/home-page.test.tsx
@@ -0,0 +1,36 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { UrlBasePaths } from "src/App";
+import i18n from "src/i18n";
+import { HomePage } from "../HomePage";
+
+const mockedUseNavigate = vi.fn();
+
+vi.mock("react-router-dom", () => ({
+  useNavigate: () => mockedUseNavigate,
+}));
+
+describe("home page", () => {
+  beforeEach(async () => {
+    await i18n.changeLanguage("cimode");
+  });
+
+  test("contains translations", () => {
+    const { container } = render(<HomePage />);
+    const openNewForButton = screen.getByRole("button");
+
+    expect(container).toHaveTextContent(/homePage.title/);
+    expect(openNewForButton).toHaveTextContent(/homePage.openNewFormButton/);
+  });
+
+  test("clicks open new form button", async () => {
+    const user = userEvent.setup();
+    render(<HomePage />);
+
+    const openNewForButton = screen.getByRole("button");
+    await user.click(openNewForButton);
+
+    expect(mockedUseNavigate).toHaveBeenCalledOnce();
+    expect(mockedUseNavigate).toHaveBeenLastCalledWith(UrlBasePaths.PROBAND_FORM);
+  });
+});
