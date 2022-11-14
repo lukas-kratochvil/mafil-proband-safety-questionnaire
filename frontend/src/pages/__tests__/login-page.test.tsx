87e8ccd733c3fc6e902b81879f38ce24d427f8a3
Lukáš Kratochvíl <lkratochvil57@gmail.com>
2022-11-14 17:57:30 +0100

    feat: created login page tests

---
+++ frontend/src/pages/__tests__/login-page.test.tsx
@@ -0,0 +1,16 @@
+import { render } from "@testing-library/react";
+import i18n from "src/i18n";
+import { LoginPage } from "../LoginPage";
+
+describe("login page", () => {
+  beforeEach(async () => {
+    await i18n.changeLanguage("cimode");
+  });
+
+  test("contains translations", () => {
+    const { container } = render(<LoginPage />);
+
+    expect(container).toHaveTextContent(/loginPage.title/);
+    expect(container).toHaveTextContent(/loginPage.loginText/);
+  });
+});
