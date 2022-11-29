089da4b5d21715668d715753f3e237e966b7ea79
Lukáš Kratochvíl <lkratochvil57@gmail.com>
2022-11-29 17:35:39 +0100

    feat: added eslint nestjs extension

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -4,8 +4,11 @@ module.exports = {
     jest: true,
   },
   extends: [
+    "plugin:@darraghor/nestjs-typed/recommended",
+    "plugin:@darraghor/nestjs-typed/no-swagger",
     "../.eslintrc.js", // this has to be the last extension!!
   ],
+  plugins: ["@darraghor/nestjs-typed"],
   parserOptions: {
     project: "tsconfig.json",
     tsconfigRootDir : __dirname,
