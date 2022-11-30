ab99735f1d3641dfc22f6c8438a63b0430f26547
Lukáš Kratochvíl <lkratochvil57@gmail.com>
2022-11-30 11:13:03 +0100

    feat: added ConfigService to work with env variables

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,13 @@
+import { ConfigService } from "@nestjs/config/dist";
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>("PORT") as number;
+  console.log(`NestJS server is listening on: http://localhost:${port}`);
+  await app.listen(port);
 }
+
 bootstrap();
