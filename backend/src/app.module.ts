cc949d22ceeea18ad4115c05dd25db1b629877ad
Lukáš Kratochvíl <lkratochvil57@gmail.com>
2022-12-11 20:10:59 +0100

    feat: configured GraphQL API

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,21 @@
+import { ApolloDriver, ApolloDriverConfig } from "@nestjs/apollo";
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
+import { GraphQLModule } from "@nestjs/graphql";
+import { GraphQLConfigService } from "@graphql/graphql-config.service";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+import { OperatorModule } from "./operator/operator.module";
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(),
+    GraphQLModule.forRootAsync<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      imports: [ConfigModule],
+      useClass: GraphQLConfigService,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ backend/src/graphql/graphql-config.service.ts
@@ -0,0 +1,18 @@
+import * as path from "path";
+import { ApolloDriverConfig } from "@nestjs/apollo";
+import { Injectable } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { GqlOptionsFactory } from "@nestjs/graphql";
+
+@Injectable()
+export class GraphQLConfigService implements GqlOptionsFactory {
+  constructor(private readonly configService: ConfigService) {}
+
+  createGqlOptions(): ApolloDriverConfig {
+    return {
+      autoSchemaFile: path.join(process.cwd(), "src/graphql/generated-schema.gql"),
+      debug: this.configService.get<string>("NODE_ENV") === "development",
+      playground: this.configService.get<string>("NODE_ENV") === "development",
+    };
+  }
+}
