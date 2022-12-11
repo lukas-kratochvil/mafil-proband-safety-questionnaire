cf31f2697a77ff832fce731db15b9e3aec56fae0
Lukáš Kratochvíl <lkratochvil57@gmail.com>
2022-12-11 19:53:51 +0100

    fix: code fixes

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,4 +1,4 @@
-import { ConfigService } from "@nestjs/config/dist";
+import { ConfigService } from "@nestjs/config";
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 
