dc4d3bee8b19d2212fb06b424e84892ddf20ae25
Lukáš Kratochvíl <lkratochvil57@gmail.com>
2022-12-11 20:12:59 +0100

    feat: edited eslint rule to ignore that GraphQLConfigService isn't included in any of the module providers

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -13,4 +13,13 @@ module.exports = {
     project: "tsconfig.json",
     tsconfigRootDir : __dirname,
   },
+  rules: {
+    "@darraghor/nestjs-typed/injectable-should-be-provided": [
+      "warn",
+      {
+          src: ["src/**/*.ts"],
+          filterFromPaths: ["graphql-config", ".spec."],
+      },
+    ],
+  }
 };
