269b7bd118a134a80424f4667ce3ca8b55c0e75d
Lukáš Kratochvíl <lkratochvil57@gmail.com>
2023-01-28 13:47:45 +0100

    fix: centered PageContainer content

--- frontend/src/pages/PageContainer.tsx
+++ frontend/src/pages/PageContainer.tsx
@@ -18,8 +18,8 @@ export const PageContainer = ({ children, centerize, isTablePage }: PropsWithChi
         <Stack
           spacing="1.5rem"
           sx={{
-            // content height is minus 4rem header and minus 3rem navigation (if viewed in the auth version)
-            height: centerize ? `calc(100vh - 4rem ${operator === undefined ? "" : "- 3rem"})` : undefined,
+            // content height is minus 4rem header and minus 3rem navigation (if viewed in the auth version).. added both sizes 2x so that the content is centered
+            height: centerize ? `calc(100vh - 8rem ${operator === undefined ? "" : "- 6rem"})` : undefined,
             width: "100%",
             maxWidth: ({ breakpoints }) => (isTablePage ? "95%" : breakpoints.values.md),
             marginX: "auto",
