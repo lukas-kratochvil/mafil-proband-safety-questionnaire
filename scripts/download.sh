04da77e6189464f47ddd2a7d9a140a198c8f7814
Lukáš Kratochvíl <lkratochvil57@gmail.com>
2023-02-26 12:36:30 +0100

    feat: refactored download script

--- scripts/download.sh
+++ scripts/download.sh
@@ -4,6 +4,9 @@
 # Download the files necessary to install or update the app
 #------------------------------------------------------------------------------------
 
+# Change this path depending on where you want to download the files
+DEST_BASE_PATH="../.."
+
 while [ true ]; do
   echo "Which environment do you want to deploy on? Options are: 'devel' or 'prod'."
   read -p "Environment: " ENVIRONMENT
@@ -25,18 +28,15 @@ while [ true ]; do
   echo
 done
 
-DEST_FOLDER_NAME="mafil_psq_$ENVIRONMENT"
-
-# Change this path depending on where you want to download the files
-DEST_FOLDER_PATH="../../$DEST_FOLDER_NAME"
+DEST_DIR_PATH="$DEST_BASE_PATH/mafil_psq_${ENVIRONMENT}"
 
 echo
 echo "Creating deploy folder at: $(
-  cd "$(dirname "$DEST_FOLDER_PATH")"
+  cd "$(dirname "$DEST_DIR_PATH")"
   pwd
-)/$(basename "$DEST_FOLDER_PATH")"
-rm -r $DEST_FOLDER_PATH
-mkdir $DEST_FOLDER_PATH
+)/$(basename "$DEST_DIR_PATH")"
+rm -r $DEST_DIR_PATH
+mkdir $DEST_DIR_PATH
 echo "> DONE"
 
 GITHUB_FILE_URL="https://raw.githubusercontent.com/lukas-kratochvil/mafil-proband-safety-questionnaire/$GIT_BRANCH"
@@ -45,12 +45,12 @@ ENV_EXAMPLE_FILE=".env.example"
 SETUP_ENV_FILE="scripts/setup_env.sh"
 
 echo "Downloading latest $DOCKER_COMPOSE_FILE from the $GIT_BRANCH branch of the GitHub repo…"
-(curl -fkSs -o "$DEST_FOLDER_PATH/$DOCKER_COMPOSE_FILE" "$GITHUB_FILE_URL/$DOCKER_COMPOSE_FILE") && echo "> DONE" || echo "> File not found!"
+(curl -fkSs -o "$DEST_DIR_PATH/$DOCKER_COMPOSE_FILE" "$GITHUB_FILE_URL/$DOCKER_COMPOSE_FILE") && echo "> DONE" || echo "> File not found!"
 
 echo "Downloading latest $ENV_EXAMPLE_FILE from the $GIT_BRANCH branch of the GitHub repo…"
-(curl -fkSs -o "$DEST_FOLDER_PATH/.env" "$GITHUB_FILE_URL/$ENV_EXAMPLE_FILE") && echo "> DONE" || echo "> File not found!"
+(curl -fkSs -o "$DEST_DIR_PATH/.env" "$GITHUB_FILE_URL/$ENV_EXAMPLE_FILE") && echo "> DONE" || echo "> File not found!"
 
 echo "Downloading latest $SETUP_ENV_FILE from the $GIT_BRANCH branch of the GitHub repo…"
-(curl -fkSs -o "$DEST_FOLDER_PATH/$SETUP_ENV_FILE" "$GITHUB_FILE_URL/$SETUP_ENV_FILE") && echo "> DONE" || echo "> File not found!"
+(curl -fkSs -o "$DEST_DIR_PATH/$SETUP_ENV_FILE" "$GITHUB_FILE_URL/$SETUP_ENV_FILE") && echo "> DONE" || echo "> File not found!"
 
 echo "Change .env file variables to satisfy your needs and then copy these files to your server."
