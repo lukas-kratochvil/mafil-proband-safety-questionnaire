4675a9db078bc8dee93cb019be47d58730900c04
Lukáš Kratochvíl <lkratochvil57@gmail.com>
2023-02-26 16:29:32 +0100

    feat: removed setup_env script due to the fact that the installation is platform dependent

--- scripts/download.sh
+++ scripts/download.sh
@@ -4,7 +4,6 @@
 # Download the files necessary to install the app on chosen environment:
 #   1. docker-compose.ENV.yml - defines app services as containers that will run in the Docker
 #   2. .env - contains configuration for the app services
-#   3. setup_env.sh - will install all the required apps i.e. docker, docker-compose and other required programs
 #-----------------------------------------------------------------------------------------------------------------------
 
 # Change this path depending on where you want to download the files
@@ -46,15 +45,11 @@ echo "> DONE"
 GITHUB_FILE_URL="https://raw.githubusercontent.com/lukas-kratochvil/mafil-proband-safety-questionnaire/$GIT_BRANCH"
 DOCKER_COMPOSE_FILE="docker-compose.$ENVIRONMENT.yml"
 ENV_EXAMPLE_FILE=".env.example"
-SETUP_ENV_FILE="setup_env.sh"
 
 echo "Downloading latest $DOCKER_COMPOSE_FILE from the $GIT_BRANCH branch of the GitHub repo…"
 (curl -fkSs -o "$DEST_DIR_PATH/$DOCKER_COMPOSE_FILE" "$GITHUB_FILE_URL/$DOCKER_COMPOSE_FILE") && echo "> DONE" || echo "> File not found!"
 
 echo "Downloading latest $ENV_EXAMPLE_FILE from the $GIT_BRANCH branch of the GitHub repo…"
 (curl -fkSs -o "$DEST_DIR_PATH/.env" "$GITHUB_FILE_URL/$ENV_EXAMPLE_FILE") && echo "> DONE" || echo "> File not found!"
-
-echo "Downloading latest $SETUP_ENV_FILE from the $GIT_BRANCH branch of the GitHub repo…"
-(curl -fkSs -o "$DEST_DIR_PATH/$SETUP_ENV_FILE" "$GITHUB_FILE_URL/scripts/$SETUP_ENV_FILE") && echo "> DONE" || echo "> File not found!"
 
 echo "Change .env file variables to satisfy your needs and then copy these files to your server."
