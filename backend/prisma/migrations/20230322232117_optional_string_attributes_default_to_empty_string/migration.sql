99c6f293fc371fe788ddea7e75b880b97716a414
Lukáš Kratochvíl <lkratochvil57@gmail.com>
2023-03-23 00:23:37 +0100

    feat: Prisma schema - optional string attributes default to the empty string

---
+++ backend/prisma/migrations/20230322232117_optional_string_attributes_default_to_empty_string/migration.sql
@@ -0,0 +1,9 @@
+-- AlterTable
+ALTER TABLE "Answer" ALTER COLUMN "comment" SET DEFAULT '';
+
+-- AlterTable
+ALTER TABLE "ProbandInfo" ALTER COLUMN "email" SET DEFAULT '',
+ALTER COLUMN "phone" SET DEFAULT '';
+
+-- AlterTable
+ALTER TABLE "VisitForm" ALTER COLUMN "visitId" SET DEFAULT '';
