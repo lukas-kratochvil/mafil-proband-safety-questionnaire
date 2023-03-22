54b1877ef4ceee3b568d49f07470bf66e277c1e2
Lukáš Kratochvíl <lkratochvil57@gmail.com>
2023-03-23 00:28:17 +0100

    fix: Prisma schema - removed visitId attribute in the VisitForm entity

---
+++ backend/prisma/migrations/20230322232635_removed_visit_id_from_the_visit_form_entity/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `visitId` on the `VisitForm` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "VisitForm" DROP COLUMN "visitId";
