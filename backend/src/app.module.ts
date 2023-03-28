a44017e6583935931f174af0f1f5db5fd5b5cd2e
Lukáš Kratochvíl <lkratochvil57@gmail.com>
2023-03-28 23:50:43 +0200

    fix: removed unnecessary GraphQL modules from the AppModule

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,15 +2,11 @@ import { ApolloDriver, ApolloDriverConfig } from "@nestjs/apollo";
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import { GraphQLModule } from "@nestjs/graphql";
-import { GenderTranslationModule } from "./api/gender-translation/gender-translation.module";
 import { GenderModule } from "./api/gender/gender.module";
 import { GraphQLConfigService } from "./api/graphql/graphql-config.service";
-import { HandednessTranslationModule } from "./api/handedness-translation/handedness-translation.module";
 import { HandednessModule } from "./api/handedness/handedness.module";
-import { LanguageModule } from "./api/language/language.module";
 import { NativeLanguageModule } from "./api/native-language/native-language.module";
 import { OperatorModule } from "./api/operator/operator.module";
-import { QuestionTranslationModule } from "./api/question-translation/question-translation.module";
 import { QuestionModule } from "./api/question/question.module";
 import { VisitFormModule } from "./api/visit-form/visit-form.module";
 
@@ -22,16 +18,12 @@ import { VisitFormModule } from "./api/visit-form/visit-form.module";
       imports: [ConfigModule],
       useClass: GraphQLConfigService,
     }),
+    GenderModule,
+    HandednessModule,
+    NativeLanguageModule,
     OperatorModule,
     QuestionModule,
-    QuestionTranslationModule,
-    LanguageModule,
-    NativeLanguageModule,
     VisitFormModule,
-    GenderModule,
-    GenderTranslationModule,
-    HandednessModule,
-    HandednessTranslationModule,
   ],
 })
 export class AppModule {}
