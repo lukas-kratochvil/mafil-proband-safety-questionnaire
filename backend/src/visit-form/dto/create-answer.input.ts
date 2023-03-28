3671e09a13b6d7e37d45fc1e803bb3d2714bd7d8
Lukáš Kratochvíl <lkratochvil57@gmail.com>
2023-03-28 15:55:27 +0200

    feat: edited comment

--- backend/src/visit-form/dto/create-answer.input.ts
+++ backend/src/visit-form/dto/create-answer.input.ts
@@ -4,7 +4,7 @@ import { AnswerEntity } from "@app/visit-form/entities/answer.entity";
 @InputType()
 export class CreateAnswerInput extends IntersectionType(
   PickType(AnswerEntity, ["questionId", "answer"]),
-  // comment field is used when duplicating existing visit form
+  // comment field is used when duplicating existing visit form that is going for approval
   PartialType(PickType(AnswerEntity, ["comment"])),
   InputType
 ) {}
