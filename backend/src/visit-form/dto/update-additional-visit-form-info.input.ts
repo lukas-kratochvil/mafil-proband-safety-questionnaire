a0de1810bcc59c36820cadd9172784ce8a33419b
Lukáš Kratochvíl <lkratochvil57@gmail.com>
2023-03-28 21:31:47 +0200

    fix: corrected UpdateAdditionalVisitFormInfoInput GraphQL input type

--- backend/src/visit-form/dto/update-additional-visit-form-info.input.ts
+++ backend/src/visit-form/dto/update-additional-visit-form-info.input.ts
@@ -1,8 +1,5 @@
-import { InputType, PartialType, PickType } from "@nestjs/graphql";
+import { InputType, PartialType } from "@nestjs/graphql";
 import { CreateAdditionalVisitFormInfoInput } from "./create-additional-visit-form-info.input";
 
 @InputType()
-export class UpdateAdditionalVisitFormInfoInput extends PartialType(
-  PickType(CreateAdditionalVisitFormInfoInput, ["projectId", "projectAcronym", "deviceId", "deviceName", "measuredAt"]),
-  InputType
-) {}
+export class UpdateAdditionalVisitFormInfoInput extends PartialType(CreateAdditionalVisitFormInfoInput, InputType) {}
