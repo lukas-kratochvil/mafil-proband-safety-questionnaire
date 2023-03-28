d640e0160b2034326ea15f22957938645b0c42e6
Lukáš Kratochvíl <lkratochvil57@gmail.com>
2023-03-29 00:34:06 +0200

    feat: created GraphQL module importing endpoint modules that can be accessed

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,13 +2,8 @@ import { ApolloDriver, ApolloDriverConfig } from "@nestjs/apollo";
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import { GraphQLModule } from "@nestjs/graphql";
-import { GenderModule } from "./api/gender/gender.module";
+import { GraphQLApiModule } from "./api/graphql/graphql-api.module";
 import { GraphQLConfigService } from "./api/graphql/graphql-config.service";
-import { HandednessModule } from "./api/handedness/handedness.module";
-import { NativeLanguageModule } from "./api/native-language/native-language.module";
-import { OperatorModule } from "./api/operator/operator.module";
-import { QuestionModule } from "./api/question/question.module";
-import { VisitFormModule } from "./api/visit-form/visit-form.module";
 
 @Module({
   imports: [
@@ -18,12 +13,7 @@ import { VisitFormModule } from "./api/visit-form/visit-form.module";
       imports: [ConfigModule],
       useClass: GraphQLConfigService,
     }),
-    GenderModule,
-    HandednessModule,
-    NativeLanguageModule,
-    OperatorModule,
-    QuestionModule,
-    VisitFormModule,
+    GraphQLApiModule,
   ],
 })
 export class AppModule {}
