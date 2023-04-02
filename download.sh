e5abbc6d684c6ef3f45a95ffe159fac1d4653580
Lukáš Kratochvíl <lkratochvil57@gmail.com>
2023-04-02 23:16:48 +0200

    feat: refactoring - edited download.sh script

--- download.sh
+++ download.sh
@@ -6,8 +6,8 @@
 #   2. .env - contains configuration for the app services
 #-----------------------------------------------------------------------------------------------------------------------
 
-# Change this path depending on where you want to download the files
-DEST_BASE_PATH="../.."
+# TODO: change this path depending on where you want to download the files
+DEST_BASE_PATH=".."
 
 while [ true ]; do
   echo "Which environment do you want to deploy on? Options are: 'devel' or 'prod'."
@@ -49,7 +49,7 @@ ENV_EXAMPLE_FILE=".env.example"
 echo "Downloading latest $DOCKER_COMPOSE_FILE from the $GIT_BRANCH branch of the GitHub repo…"
 (curl -fkSs -o "$DEST_DIR_PATH/$DOCKER_COMPOSE_FILE" "$GITHUB_FILE_URL/$DOCKER_COMPOSE_FILE") && echo "> DONE" || echo "> File not found!"
 
-echo "Downloading latest $ENV_EXAMPLE_FILE from the $GIT_BRANCH branch of the GitHub repo…"
+echo "Downloading latest .env from the $GIT_BRANCH branch of the GitHub repo…"
 (curl -fkSs -o "$DEST_DIR_PATH/.env" "$GITHUB_FILE_URL/$ENV_EXAMPLE_FILE") && echo "> DONE" || echo "> File not found!"
 
 echo "Change .env file variables to satisfy your needs and then copy these files to your server."
