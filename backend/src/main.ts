dfefdc88f47fab747593afbe9b22f0347e9c27a1
Lukáš Kratochvíl <lkratochvil57@gmail.com>
2023-04-03 19:30:20 +0200

    feat: allowing all CORS origins

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,17 +5,21 @@ import { createWinstonLogger } from "./log/winston-logger";
 
 async function bootstrap() {
   const logger = createWinstonLogger();
-  const app = await NestFactory.create(AppModule, { logger });
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+    logger,
+  });
   const configService = app.get(ConfigService);
 
+  // TODO: use CORS? Origins 'localhost' and '127.0.0.1' are different.
   // CORS
-  const webUrl = configService.get<string>("WEB_URL");
-  if (webUrl === undefined) {
-    const errorMsg = "MAFIL-PSQ web app URL is not defined! Shutting down…";
-    logger.error(errorMsg);
-    throw new Error(errorMsg);
-  }
-  app.enableCors({ origin: [webUrl] });
+  // const webUrl = configService.get<string>("WEB_URL");
+  // if (webUrl === undefined) {
+  //   const errorMsg = "MAFIL-PSQ web app URL is not defined! Shutting down…";
+  //   logger.error(errorMsg);
+  //   throw new Error(errorMsg);
+  // }
+  // app.enableCors({ origin: [webUrl] });
 
   // PORT
   const port = configService.get<number>("PORT");
