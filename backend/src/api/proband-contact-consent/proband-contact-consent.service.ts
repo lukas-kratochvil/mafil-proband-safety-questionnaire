1037f48e1f5c2ad10ad3d4d174c990f3e7358d5b
Lukáš Kratochvíl <lkratochvil57@gmail.com>
2023-04-05 22:24:09 +0200

    feat: refactoring - new json localization files structure

--- backend/src/api/proband-contact-consent/proband-contact-consent.service.ts
+++ backend/src/api/proband-contact-consent/proband-contact-consent.service.ts
@@ -1,34 +1,8 @@
 import path from "path";
-import { Injectable, Logger } from "@nestjs/common";
+import { Injectable } from "@nestjs/common";
+import { IPdfCommonItemsFile, IPdfTextsFile } from "@app/pdf/interfaces";
 import { PrismaService } from "@app/prisma/prisma.service";
 import { ProbandContactConsentEntity } from "./entities/proband-contact-consent.entity";
-
-export interface IProbandContactConsentTexts {
-  title: string;
-  text1: string;
-  text2: string;
-  text3: string;
-  text4: string;
-  list: string;
-  listItem1: string;
-  listItem2: string;
-  listItem3: string;
-  listItem4: string;
-  listItem5Part1: string;
-  listItem5Part2: string;
-  text5Part1: string;
-  text5Part2: string;
-  text5Part3: string;
-}
-
-export interface IProbandContactConsentCommonItems {
-  mafilEmail: string;
-  uoouSite: string;
-  uoouEmail: string;
-  poverenecEmail: string;
-  personalInfoProtectionSite: string;
-  applicationOfDataSubjectRightsSite: string;
-}
 
 @Injectable()
 export class ProbandContactConsentService {
@@ -45,39 +19,39 @@ export class ProbandContactConsentService {
       throw new Error(`Locale '${locale}' is not supported!`);
     }
 
-    const dirPath = path.join(process.cwd(), "dist", "assets", "proband-contact-consent");
+    const dirPath = path.join(process.cwd(), "dist", "assets", "localization");
     // eslint-disable-next-line @typescript-eslint/no-var-requires
-    const texts: IProbandContactConsentTexts = require(path.join(dirPath, `${locale}.json`));
+    const texts: IPdfTextsFile = require(path.join(dirPath, `${locale}.json`));
     // eslint-disable-next-line @typescript-eslint/no-var-requires
-    const commonItems: IProbandContactConsentCommonItems = require(path.join(dirPath, "common.json"));
+    const commonItems: IPdfCommonItemsFile = require(path.join(dirPath, "common.json"));
 
     const html = `
-      <p style="margin-top: 0">${texts.text1}</p>
+      <p style="margin-top: 0">${texts.probandContactConsent.text1}</p>
       <p>
-        ${texts.text2}
+        ${texts.probandContactConsent.text2}
         <br />
-        ${texts.text3}
+        ${texts.probandContactConsent.text3}
       </p>
-      <p>${texts.text4}</p>
-      <div>${texts.list}</div>
+      <p>${texts.probandContactConsent.text4}</p>
+      <div>${texts.probandContactConsent.list}</div>
       <ul style="margin: 0">
-      <li>${texts.listItem1} <u>${commonItems.mafilEmail}</u>,</li>
-      <li>${texts.listItem2}</li>
-      <li>${texts.listItem3}</li>
-      <li>${texts.listItem4}</li>
-      <li>${texts.listItem5Part1} <u>${commonItems.uoouSite}</u>, ${texts.listItem5Part2} <u>${commonItems.uoouEmail}</u>.</li>
+      <li>${texts.probandContactConsent.listItem1} <u>${commonItems.mafilEmail}</u>,</li>
+      <li>${texts.probandContactConsent.listItem2}</li>
+      <li>${texts.probandContactConsent.listItem3}</li>
+      <li>${texts.probandContactConsent.listItem4}</li>
+      <li>${texts.probandContactConsent.listItem5Part1} <u>${commonItems.uoouSite}</u>, ${texts.probandContactConsent.listItem5Part2} <u>${commonItems.uoouEmail}</u>.</li>
       </ul>
       <p style="margin-bottom: 0">
-        ${texts.text5Part1} <u>${commonItems.poverenecEmail}</u>.
-        <br />${texts.text5Part2}
+        ${texts.probandContactConsent.text5Part1} <u>${commonItems.poverenecEmail}</u>.
+        <br />${texts.probandContactConsent.text5Part2}
         <br /><u>${commonItems.personalInfoProtectionSite}</u>.
-        <br />${texts.text5Part3}
+        <br />${texts.probandContactConsent.text5Part3}
         <br /><u>${commonItems.applicationOfDataSubjectRightsSite}</u>.
       </p>
     `;
 
     const probandContactConsent = new ProbandContactConsentEntity();
-    probandContactConsent.title = texts.title;
+    probandContactConsent.title = texts.probandContactConsent.title;
     probandContactConsent.html = html;
     return probandContactConsent;
   }
---
+++ backend/src/pdf/interfaces.ts
@@ -0,0 +1,43 @@
+export interface IPdfCommonItemsFile {
+  mafilEmail: string;
+  uoouSite: string;
+  uoouEmail: string;
+  poverenecEmail: string;
+  personalInfoProtectionSite: string;
+  applicationOfDataSubjectRightsSite: string;
+}
+
+export interface IPdfTextsFile {
+  inputTitles: {
+    project: string;
+    measurementDate: string;
+    phantom: string;
+    name: string;
+    surname: string;
+    personalId: string;
+    birthdate: string;
+    gender: string;
+    nativeLanguage: string;
+    height: string;
+    weight: string;
+    visualCorrection: string;
+    handedness: string;
+  };
+  probandContactConsent: {
+    title: string;
+    text1: string;
+    text2: string;
+    text3: string;
+    text4: string;
+    list: string;
+    listItem1: string;
+    listItem2: string;
+    listItem3: string;
+    listItem4: string;
+    listItem5Part1: string;
+    listItem5Part2: string;
+    text5Part1: string;
+    text5Part2: string;
+    text5Part3: string;
+  };
+}
