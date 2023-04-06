48db2cc3a7ad24ea636302b0635a1176030a9292
Lukáš Kratochvíl <lkratochvil57@gmail.com>
2023-04-06 13:48:57 +0200

    feat: moved Nest GraphQL module into custom GraphQLApiModule

--- backend/src/api/graphql-api.module.ts
+++ backend/src/api/graphql-api.module.ts
@@ -1,5 +1,9 @@
+import { ApolloDriver, ApolloDriverConfig } from "@nestjs/apollo";
 import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { GraphQLModule } from "@nestjs/graphql";
 import { GenderModule } from "./gender/gender.module";
+import { GraphQLConfigService } from "./graphql-config.service";
 import { HandednessModule } from "./handedness/handedness.module";
 import { NativeLanguageModule } from "./native-language/native-language.module";
 import { OperatorModule } from "./operator/operator.module";
@@ -8,8 +12,13 @@ import { QuestionModule } from "./question/question.module";
 import { VisitFormModule } from "./visit-form/visit-form.module";
 
 @Module({
-  // only GraphQL resolvers imported from the modules below can be accessed via the GraphQL API
   imports: [
+    GraphQLModule.forRootAsync<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      imports: [ConfigModule],
+      useClass: GraphQLConfigService,
+    }),
+    // only GraphQL resolvers imported from the modules below can be accessed via the GraphQL API
     GenderModule,
     HandednessModule,
     NativeLanguageModule,
--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,27 +1,13 @@
-import { ApolloDriver, ApolloDriverConfig } from "@nestjs/apollo";
 import { Logger, MiddlewareConsumer, Module, NestModule, RequestMethod } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
-import { GraphQLModule } from "@nestjs/graphql";
 import { ScheduleModule } from "@nestjs/schedule";
 import { GraphQLApiModule } from "./api/graphql-api.module";
-import { GraphQLConfigService } from "./api/graphql-config.service";
 import { CronModule } from "./cron/cron.module";
 import { AuthGuardModule } from "./guards/auth/auth.module";
 import { LoggerMiddleware } from "./middleware/logger.middleware";
 
 @Module({
-  imports: [
-    ConfigModule.forRoot(),
-    GraphQLModule.forRootAsync<ApolloDriverConfig>({
-      driver: ApolloDriver,
-      imports: [ConfigModule],
-      useClass: GraphQLConfigService,
-    }),
-    GraphQLApiModule,
-    AuthGuardModule,
-    CronModule,
-    ScheduleModule.forRoot(),
-  ],
+  imports: [ConfigModule.forRoot(), GraphQLApiModule, AuthGuardModule, CronModule, ScheduleModule.forRoot()],
   providers: [Logger],
 })
 export class AppModule implements NestModule {
