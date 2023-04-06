d76cc60ea86a1397013c7d7ef17273dec25c115d
Lukáš Kratochvíl <lkratochvil57@gmail.com>
2023-04-06 17:41:47 +0200

    feat: got rid of GraphQL config service and setting up the GraphQL using factory method

--- backend/src/api/graphql-api.module.ts
+++ backend/src/api/graphql-api.module.ts
@@ -1,14 +1,16 @@
+import path from "path";
 import { ApolloDriver, ApolloDriverConfig } from "@nestjs/apollo";
 import { Module } from "@nestjs/common";
-import { ConfigModule } from "@nestjs/config";
+import { ConfigModule, ConfigService } from "@nestjs/config";
 import { GraphQLModule } from "@nestjs/graphql";
 import { GenderModule } from "./gender/gender.module";
-import { GraphQLConfigService } from "./graphql-config.service";
 import { HandednessModule } from "./handedness/handedness.module";
 import { NativeLanguageModule } from "./native-language/native-language.module";
 import { OperatorModule } from "./operator/operator.module";
 import { ProbandContactConsentModule } from "./proband-contact-consent/proband-contact-consent.module";
 import { QuestionModule } from "./question/question.module";
+import { UUID } from "./utils/scalars/uuid-scalar";
+import { Void } from "./utils/scalars/void-scalar";
 import { VisitFormModule } from "./visit-form/visit-form.module";
 
 @Module({
@@ -16,7 +18,17 @@ import { VisitFormModule } from "./visit-form/visit-form.module";
     GraphQLModule.forRootAsync<ApolloDriverConfig>({
       driver: ApolloDriver,
       imports: [ConfigModule],
-      useClass: GraphQLConfigService,
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        autoSchemaFile: path.join(process.cwd(), "graphql-schema.gql"),
+        // passing original request and response objects into the GraphQL context
+        context: ({ req, res }: { req: any; res: any }) => ({ req, res }),
+        resolvers: {
+          UUID: UUID,
+          Void: Void,
+        },
+        playground: config.get<string>("NODE_ENV") === "development",
+      }),
     }),
     // only GraphQL resolvers imported from the modules below can be accessed via the GraphQL API
     GenderModule,
