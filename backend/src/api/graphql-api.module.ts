1fd5f141802e29c25d283da44edd0c566a409172
Lukáš Kratochvíl <lkratochvil57@gmail.com>
2023-04-07 23:18:29 +0200

    feat: extracted GraphQL config back to custom config service file

--- backend/src/api/graphql-api.module.ts
+++ backend/src/api/graphql-api.module.ts
@@ -1,18 +1,14 @@
-import path from "path";
 import { ApolloDriver, ApolloDriverConfig } from "@nestjs/apollo";
 import { Module } from "@nestjs/common";
-import { ConfigModule, ConfigService } from "@nestjs/config";
+import { ConfigModule } from "@nestjs/config";
 import { GraphQLModule } from "@nestjs/graphql";
-import { GraphQLFormattedError } from "graphql";
-import { AppErrorExtensions, ValidationFieldErrors, VALIDATION_ERROR } from "@app/exception-handling";
 import { GenderModule } from "./gender/gender.module";
+import { GraphQLConfigService } from "./graphql-config.service";
 import { HandednessModule } from "./handedness/handedness.module";
 import { HTMLCardModule } from "./html-card/html-card.module";
 import { NativeLanguageModule } from "./native-language/native-language.module";
 import { OperatorModule } from "./operator/operator.module";
 import { QuestionModule } from "./question/question.module";
-import { UUID } from "./utils/scalars/uuid-scalar";
-import { Void } from "./utils/scalars/void-scalar";
 import { VisitFormModule } from "./visit-form/visit-form.module";
 
 @Module({
@@ -20,48 +16,7 @@ import { VisitFormModule } from "./visit-form/visit-form.module";
     GraphQLModule.forRootAsync<ApolloDriverConfig>({
       driver: ApolloDriver,
       imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (config: ConfigService) => ({
-        autoSchemaFile: path.join(process.cwd(), "graphql-schema.gql"),
-        // passing original request and response objects into the GraphQL context
-        context: ({ req, res }: { req: any; res: any }) => ({ req, res }),
-        // error formatting inspired by: https://github.com/nestjs/graphql/issues/1053#issuecomment-786972617
-        formatError: (error: GraphQLFormattedError) => {
-          if (error.message === VALIDATION_ERROR) {
-            const appErrorExtensions = error.extensions as AppErrorExtensions;
-            const validationFieldErrors: ValidationFieldErrors[] = [];
-
-            appErrorExtensions.validationErrors.forEach((validationError) => {
-              if (validationError.constraints !== undefined) {
-                const constraints: string[] = [];
-                Object.keys(validationError.constraints).forEach((constraintKey) => {
-                  constraints.push((validationError.constraints as any)[constraintKey]);
-                });
-                validationFieldErrors.push({
-                  field: validationError.property,
-                  errors: constraints,
-                });
-              }
-            });
-
-            const graphQLFormattedError: GraphQLFormattedError = {
-              message: "Validation error",
-              extensions: {
-                code: VALIDATION_ERROR,
-                errors: validationFieldErrors,
-              },
-            };
-            return graphQLFormattedError;
-          } else {
-            return error;
-          }
-        },
-        resolvers: {
-          UUID: UUID,
-          Void: Void,
-        },
-        playground: config.get<string>("NODE_ENV") === "development",
-      }),
+      useClass: GraphQLConfigService,
     }),
     // only GraphQL resolvers imported from the modules below can be accessed via the GraphQL API
     GenderModule,
---
+++ backend/src/api/graphql-config.service.ts
@@ -0,0 +1,59 @@
+import * as path from "path";
+import { ApolloDriverConfig } from "@nestjs/apollo";
+import { Injectable } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { GqlOptionsFactory } from "@nestjs/graphql";
+import { GraphQLFormattedError } from "graphql";
+import { AppErrorExtensions, ValidationFieldErrors, VALIDATION_ERROR } from "@app/exception-handling";
+import { UUID } from "./utils/scalars/uuid-scalar";
+import { Void } from "./utils/scalars/void-scalar";
+
+@Injectable()
+// eslint-disable-next-line @darraghor/nestjs-typed/injectable-should-be-provided
+export class GraphQLConfigService implements GqlOptionsFactory {
+  constructor(private readonly config: ConfigService) {}
+
+  createGqlOptions(): ApolloDriverConfig {
+    return {
+      autoSchemaFile: path.join(process.cwd(), "graphql-schema.gql"),
+      // passing original request and response objects into the GraphQL context
+      context: ({ req, res }: { req: any; res: any }) => ({ req, res }),
+      // error formatting inspired by: https://github.com/nestjs/graphql/issues/1053#issuecomment-786972617
+      formatError: (error: GraphQLFormattedError) => {
+        if (error.message === VALIDATION_ERROR) {
+          const appErrorExtensions = error.extensions as AppErrorExtensions;
+          const validationFieldErrors: ValidationFieldErrors[] = [];
+
+          appErrorExtensions.validationErrors.forEach((validationError) => {
+            if (validationError.constraints !== undefined) {
+              const constraints: string[] = [];
+              Object.keys(validationError.constraints).forEach((constraintKey) => {
+                constraints.push((validationError.constraints as any)[constraintKey]);
+              });
+              validationFieldErrors.push({
+                field: validationError.property,
+                errors: constraints,
+              });
+            }
+          });
+
+          const graphQLFormattedError: GraphQLFormattedError = {
+            message: "Validation error",
+            extensions: {
+              code: VALIDATION_ERROR,
+              errors: validationFieldErrors,
+            },
+          };
+          return graphQLFormattedError;
+        }
+
+        return error;
+      },
+      resolvers: {
+        UUID: UUID,
+        Void: Void,
+      },
+      playground: this.config.get<string>("NODE_ENV") === "development",
+    };
+  }
+}
