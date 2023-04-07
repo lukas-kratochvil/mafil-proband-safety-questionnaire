82a008ed833a82a842f7032a61a4c180366ab2ab
Lukáš Kratochvíl <lkratochvil57@gmail.com>
2023-04-07 11:36:26 +0200

    fix: removed validation of HTMLCardEntity

--- backend/src/api/html-card/entities/html-card.entity.ts
+++ backend/src/api/html-card/entities/html-card.entity.ts
@@ -1,13 +1,10 @@
 import { Field, ObjectType } from "@nestjs/graphql";
-import { IsString } from "class-validator";
 
 @ObjectType()
 export class HTMLCardEntity {
   @Field()
-  @IsString()
   title: string;
 
   @Field()
-  @IsString()
   html: string;
 }
