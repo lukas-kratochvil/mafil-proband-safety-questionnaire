b1a91c6a03c588a98c83d1d2d508101de84470fc
Lukáš Kratochvíl <lkratochvil57@gmail.com>
2023-04-07 23:43:56 +0200

    fix: added validation decorators to HTMLCard classes so it passes validation - else validation error will be thrown due to set 'forbidUnknownValues' option in the global ValidationPipe

--- backend/src/api/html-card/dto/proband-contact-request.args.ts
+++ backend/src/api/html-card/dto/proband-contact-request.args.ts
@@ -1,19 +1,25 @@
 import { ArgsType, Field } from "@nestjs/graphql";
+import { IsString } from "class-validator";
 
 @ArgsType()
 export class ProbandContactRequestArgs {
+  @IsString()
   @Field()
   locale: string;
 
+  @IsString()
   @Field()
   name: string;
 
+  @IsString()
   @Field()
   surname: string;
 
+  @IsString()
   @Field()
   birthdateStr: string;
 
+  @IsString()
   @Field()
   currentDateStr: string;
 }
--- backend/src/api/html-card/entities/html-card.entity.ts
+++ backend/src/api/html-card/entities/html-card.entity.ts
@@ -1,10 +1,13 @@
 import { Field, ObjectType } from "@nestjs/graphql";
+import { IsString } from "class-validator";
 
 @ObjectType()
 export class HTMLCardEntity {
+  @IsString()
   @Field()
   title: string;
 
+  @IsString()
   @Field()
   html: string;
 }
