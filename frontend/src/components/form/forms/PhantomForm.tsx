37918b8e518bd5e0bc51009247348dd267606e73
Lukáš Kratochvíl <lkratochvil57@gmail.com>
2023-04-08 21:18:12 +0200

    fix: removed questions from the PhantomForm

--- frontend/src/components/form/forms/PhantomForm.tsx
+++ frontend/src/components/form/forms/PhantomForm.tsx
@@ -1,33 +1,17 @@
-import { useQuery } from "@tanstack/react-query";
-import { useEffect } from "react";
-import { useFormContext } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 import { IFormButtonsProps } from "@app/components/form/components/FormButtons";
 import { FormProbandInfo } from "@app/components/form/components/FormProbandInfo";
 import { FormProjectInfo } from "@app/components/form/components/FormProjectInfo";
 import { createNewVisitFromFormData } from "@app/components/form/util/utils.dev";
 import { dummyVisits } from "@app/data/visit_data";
-import { AnswerOption, FormPropType } from "@app/model/form";
+import { FormPropType } from "@app/model/form";
 import { VisitStateDEV } from "@app/model/visit";
 import { RoutingPaths } from "@app/routing-paths";
-import { fetchCurrentQuestions } from "@app/util/server_API/fetch";
 import { getBackButtonProps } from "@app/util/utils";
 import { FormContainer } from "./FormContainer";
 
 export const PhantomForm = () => {
   const navigate = useNavigate();
-  const { setValue } = useFormContext<FormPropType>();
-
-  const {
-    data: questions,
-    isLoading,
-    isError,
-  } = useQuery({
-    queryKey: ["currentQuestions"],
-    queryFn: fetchCurrentQuestions,
-    staleTime: Infinity,
-    cacheTime: Infinity,
-  });
 
   const formButtons: IFormButtonsProps = {
     submitButtonProps: {
@@ -42,25 +26,10 @@ export const PhantomForm = () => {
     buttonsProps: [getBackButtonProps(navigate, "form.common.buttons.cancel")],
   };
 
-  useEffect(() => {
-    if (questions !== undefined) {
-      setValue(
-        "answers",
-        questions.map((qac, index) => ({
-          index,
-          questionId: qac.id,
-          partNumber: qac.partNumber,
-          answer: AnswerOption.NO,
-          comment: "",
-        }))
-      );
-    }
-  }, [questions, setValue]);
-
   return (
     <FormContainer
-      isLoading={isLoading}
-      isError={isError}
+      isLoading={false}
+      isError={false}
       buttons={formButtons}
     >
       <FormProjectInfo isPhantom />
