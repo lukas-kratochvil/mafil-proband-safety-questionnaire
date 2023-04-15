5b1ac144d7e36837db75143409f5f4af5f338468
Lukáš Kratochvíl <lkratochvil57@gmail.com>
2023-04-16 01:40:52 +0200

    feat: removed InteractingTable and moved the core functionality to the RecentVisitsTablePage

--- frontend/src/pages/RecentVisitsTablePage.tsx
+++ frontend/src/pages/RecentVisitsTablePage.tsx
@@ -1,80 +1,89 @@
+import { useQuery } from "@tanstack/react-query";
 import { compareAsc, format, parse } from "date-fns";
-import { MRT_ColumnDef as MRTColumnDef, MRT_Row as MRTRow } from "material-react-table";
+import MaterialReactTable, { MRT_ColumnDef as MRTColumnDef, MRT_Row as MRTRow } from "material-react-table";
+import { useMemo } from "react";
 import { useTranslation } from "react-i18next";
-import { InteractingTable } from "@app/components/table/InteractingTable";
 import { RecentVisitsTableActionButtons } from "@app/components/table/actions/RecentVisitsTableActionButtons";
+import { defaultTableProps } from "@app/components/table/default-table-props";
 import { defaultNS } from "@app/i18n";
 import { IVisit } from "@app/model/visit";
 import { fetchRecentVisits } from "@app/util/mafildb_API/fetch";
+import { PageContainer } from "./PageContainer";
 
 const processedDateFormat = "d.M.y H:mm";
 
 const RecentVisitsTablePage = () => {
-  const { t } = useTranslation(defaultNS, { keyPrefix: "recentVisitsTablePage.header" });
+  const { t } = useTranslation(defaultNS, { keyPrefix: "recentVisitsTablePage" });
+  const {
+    data: visits,
+    isFetching,
+    isLoading,
+    isError,
+  } = useQuery({ queryKey: ["recentVisitsTablePage"], queryFn: fetchRecentVisits });
 
-  const header: MRTColumnDef<IVisit>[] = [
-    {
-      accessorKey: "visitId",
-      header: "Visit ID",
-      size: 100,
-    },
-    {
-      accessorFn: (visit) => `${visit.surname}, ${visit.name}`,
-      id: "proband",
-      header: t("proband"),
-      minSize: 150,
-    },
-    {
-      accessorKey: "projectInfo.projectAcronym",
-      header: t("project"),
-      minSize: 300,
-    },
-    {
-      accessorKey: "projectInfo.deviceName",
-      header: t("device"),
-      maxSize: 0,
-    },
-    {
-      // TODO: change 'visit.createdAt' for some other attribute - firstly, we need to solve which attribute should be created for this purpose
-      accessorFn: (visit) => format(visit.createdAt, processedDateFormat),
-      id: "processedDate",
-      header: t("processedDate"),
-      sortingFn: (rowA, rowB, columnId) =>
-        compareAsc(
-          parse(`${rowA.getValue(columnId)}`, processedDateFormat, new Date()),
-          parse(`${rowB.getValue(columnId)}`, processedDateFormat, new Date())
-        ),
-      maxSize: 0,
-    },
-    {
-      accessorFn: () => "MUNI_operator",
-      id: "processUser",
-      header: t("operatorProcessed"),
-      maxSize: 0,
-    },
-    {
-      accessorFn: (visit) => (visit.projectInfo.isPhantom ? "Dokončeno" : visit.state),
-      id: "state",
-      header: t("state"),
-      maxSize: 0,
-    },
-    {
-      id: "actionButtons",
-      header: t("actions"),
-      columnDefType: "display", // turns off data column features like sorting, filtering, etc.
-      // eslint-disable-next-line react/no-unstable-nested-components
-      Cell: ({ row }: { row: MRTRow<IVisit> }) => <RecentVisitsTableActionButtons visitId={row.original.id} />,
-      minSize: 300,
-    },
-  ];
+  const columns = useMemo<MRTColumnDef<IVisit>[]>(
+    () => [
+      {
+        accessorKey: "visitId",
+        header: "Visit ID",
+        size: 100,
+      },
+      {
+        accessorFn: (visit) => `${visit.surname}, ${visit.name}`,
+        id: "proband",
+        header: t("header.proband"),
+        minSize: 150,
+      },
+      {
+        accessorKey: "projectInfo.projectAcronym",
+        header: t("header.project"),
+        minSize: 300,
+      },
+      {
+        accessorKey: "projectInfo.deviceName",
+        header: t("header.device"),
+        maxSize: 0,
+      },
+      {
+        // TODO: change 'visit.createdAt' for some other attribute - firstly, we need to solve which attribute should be created for this purpose
+        accessorFn: (visit) => format(visit.createdAt, processedDateFormat),
+        id: "processedDate",
+        header: t("header.processedDate"),
+        sortingFn: (rowA, rowB, columnId) =>
+          compareAsc(
+            parse(`${rowA.getValue(columnId)}`, processedDateFormat, new Date()),
+            parse(`${rowB.getValue(columnId)}`, processedDateFormat, new Date())
+          ),
+        maxSize: 0,
+      },
+      {
+        accessorFn: () => "MUNI_operator",
+        id: "processUser",
+        header: t("header.operatorProcessed"),
+        maxSize: 0,
+      },
+      {
+        accessorFn: (visit) => (visit.projectInfo.isPhantom ? "Dokončeno" : visit.state),
+        id: "state",
+        header: t("header.state"),
+        maxSize: 0,
+      },
+      {
+        id: "actionButtons",
+        header: t("header.actions"),
+        columnDefType: "display", // turns off data column features like sorting, filtering, etc.
+        // eslint-disable-next-line react/no-unstable-nested-components
+        Cell: ({ row }: { row: MRTRow<IVisit> }) => <RecentVisitsTableActionButtons visitId={row.original.id} />,
+        minSize: 300,
+      },
+    ],
+    [t]
+  );
 
   return (
-    <InteractingTable
-      titleLocalizationKey="recentVisitsTablePage.title"
-      header={header}
-      queryKey="recentVisits"
-      fetchVisits={fetchRecentVisits}
-    />
+    <PageContainer isTablePage>
+      <MaterialReactTable {...defaultTableProps(t("title"), columns, visits, isFetching, isLoading, isError)} />
+    </PageContainer>
   );
 };
 
