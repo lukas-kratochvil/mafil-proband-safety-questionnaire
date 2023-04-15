fd1286f71d9642028339e5790aaec211f16a26c1
Lukáš Kratochvíl <lkratochvil57@gmail.com>
2023-04-15 19:27:01 +0200

    feat: added check in the visit form for multiple updates to the same state

--- backend/src/api/visit-form/visit-form.service.ts
+++ backend/src/api/visit-form/visit-form.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@nestjs/common";
+import { BadRequestException, Injectable } from "@nestjs/common";
 import { Prisma, VisitFormState } from "@prisma/client";
 import { PrismaService } from "@app/prisma/prisma.service";
 import { translationsIncludeSchema } from "../utils/utils";
@@ -99,80 +99,96 @@ export class VisitFormService {
   }
 
   async update(id: string, updateVisitFormInput: UpdateVisitFormInput): Promise<VisitFormInclude> {
-    return this.prisma.visitForm.update({
-      where: {
-        id,
-      },
-      data: {
-        state: updateVisitFormInput.state,
-        name: updateVisitFormInput.name,
-        surname: updateVisitFormInput.surname,
-        personalId: updateVisitFormInput.personalId,
-        birthdate: updateVisitFormInput.birthdate,
-        gender: updateVisitFormInput.genderId
-          ? {
-              connect: {
-                id: updateVisitFormInput.genderId,
-              },
-            }
-          : undefined,
-        nativeLanguage: updateVisitFormInput.nativeLanguageId
-          ? {
-              connect: {
-                id: updateVisitFormInput.nativeLanguageId,
-              },
-            }
-          : undefined,
-        heightCm: updateVisitFormInput.heightCm,
-        weightKg: updateVisitFormInput.weightKg,
-        handedness: updateVisitFormInput.handednessId
-          ? {
-              connect: {
-                id: updateVisitFormInput.handednessId,
-              },
-            }
-          : undefined,
-        visualCorrectionDioptre: updateVisitFormInput.visualCorrectionDioptre,
-        email: updateVisitFormInput.email,
-        phone: updateVisitFormInput.phone,
-        additionalInfo: updateVisitFormInput.additionalInfo
-          ? {
-              upsert: {
-                create: {
-                  projectId: updateVisitFormInput.additionalInfo.projectId || "",
-                  projectAcronym: updateVisitFormInput.additionalInfo.projectAcronym || "",
-                  deviceId: updateVisitFormInput.additionalInfo.deviceId || "",
-                  deviceName: updateVisitFormInput.additionalInfo.deviceName || "",
-                  measuredAt: updateVisitFormInput.additionalInfo.measuredAt || new Date(),
-                  finalizedAt: updateVisitFormInput.additionalInfo.finalizedAt || new Date(),
-                  finalizer: {
-                    connect: {
-                      id: updateVisitFormInput.additionalInfo.finalizerId,
+    return await this.prisma.$transaction(async (tx) => {
+      // check if visit form state wasn't already updated by another user
+      if (updateVisitFormInput.state) {
+        const visitForm = await tx.visitForm.findFirst({
+          where: {
+            id,
+            state: updateVisitFormInput.state,
+          },
+        });
+
+        if (visitForm) {
+          throw new BadRequestException("Visit form is already in the required state!");
+        }
+      }
+
+      return tx.visitForm.update({
+        where: {
+          id,
+        },
+        data: {
+          state: updateVisitFormInput.state,
+          name: updateVisitFormInput.name,
+          surname: updateVisitFormInput.surname,
+          personalId: updateVisitFormInput.personalId,
+          birthdate: updateVisitFormInput.birthdate,
+          gender: updateVisitFormInput.genderId
+            ? {
+                connect: {
+                  id: updateVisitFormInput.genderId,
+                },
+              }
+            : undefined,
+          nativeLanguage: updateVisitFormInput.nativeLanguageId
+            ? {
+                connect: {
+                  id: updateVisitFormInput.nativeLanguageId,
+                },
+              }
+            : undefined,
+          heightCm: updateVisitFormInput.heightCm,
+          weightKg: updateVisitFormInput.weightKg,
+          handedness: updateVisitFormInput.handednessId
+            ? {
+                connect: {
+                  id: updateVisitFormInput.handednessId,
+                },
+              }
+            : undefined,
+          visualCorrectionDioptre: updateVisitFormInput.visualCorrectionDioptre,
+          email: updateVisitFormInput.email,
+          phone: updateVisitFormInput.phone,
+          additionalInfo: updateVisitFormInput.additionalInfo
+            ? {
+                upsert: {
+                  create: {
+                    projectId: updateVisitFormInput.additionalInfo.projectId || "",
+                    projectAcronym: updateVisitFormInput.additionalInfo.projectAcronym || "",
+                    deviceId: updateVisitFormInput.additionalInfo.deviceId || "",
+                    deviceName: updateVisitFormInput.additionalInfo.deviceName || "",
+                    measuredAt: updateVisitFormInput.additionalInfo.measuredAt || new Date(),
+                    finalizedAt: updateVisitFormInput.additionalInfo.finalizedAt || new Date(),
+                    finalizer: {
+                      connect: {
+                        id: updateVisitFormInput.additionalInfo.finalizerId,
+                      },
                     },
                   },
+                  update: {
+                    ...updateVisitFormInput.additionalInfo,
+                  },
                 },
-                update: {
-                  ...updateVisitFormInput.additionalInfo,
-                },
-              },
-            }
-          : undefined,
-        answers: {
-          updateMany: updateVisitFormInput.answers
-            ? updateVisitFormInput.answers.map((answer) => ({
-                where: {
-                  // updates all the related answers having specified question ID
-                  questionId: answer.questionId,
-                },
-                data: {
-                  answer: answer.answer,
-                  comment: answer.comment,
-                },
-              }))
-            : [],
+              }
+            : undefined,
+          answers: {
+            updateMany: updateVisitFormInput.answers
+              ? updateVisitFormInput.answers.map((answer) => ({
+                  where: {
+                    // updates all the related answers having specified question ID
+                    questionId: answer.questionId,
+                  },
+                  data: {
+                    answer: answer.answer,
+                    comment: answer.comment,
+                  },
+                }))
+              : [],
+          },
         },
-      },
-      include: visitFormInclude,
+        include: visitFormInclude,
+      });
     });
   }
 
