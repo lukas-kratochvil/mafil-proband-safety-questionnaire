9e8619388d0f02827e018ecce8bef15bb7307cf6
Lukáš Kratochvíl <lkratochvil57@gmail.com>
2023-04-25 16:00:26 +0200

    feat: refactoring - renamed createServerApiCallError() variable

--- frontend/src/util/server_API/error-handling.ts
+++ frontend/src/util/server_API/error-handling.ts
@@ -2,18 +2,18 @@ import { GraphQlError } from "./response-types";
 
 // show only validation errors and hide all the others (mostly internal server errors) under one customizable message
 export const createServerApiCallError = (errors: GraphQlError[] | undefined, message: string): Error => {
-  if (errors) {
-    let errorMessage = "";
+  if (errors && errors.length > 0) {
+    let validationErrorMessage = "";
     errors.forEach((error) => {
       if (error.extensions.code === "VALIDATION_ERROR") {
         error.extensions.errors?.forEach((validationError) => {
-          errorMessage += `${validationError.field}: ${validationError.errors.join(" ")}\n`;
+          validationErrorMessage += `${validationError.field}: ${validationError.errors.join(" ")}\n`;
         });
       }
     });
 
-    if (errorMessage.length > 0) {
-      return new Error(errorMessage);
+    if (validationErrorMessage.length > 0) {
+      return new Error(validationErrorMessage);
     }
   }
 
