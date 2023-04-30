d21802bf3193deb7e3985066a9635423793286c4
Lukáš Kratochvíl <lkratochvil57@gmail.com>
2023-04-30 13:55:29 +0200

    feat: added comments in the frontend Axios config

--- frontend/src/axios-config.ts
+++ frontend/src/axios-config.ts
@@ -3,9 +3,11 @@ import { transformDateStringToDate } from "./axios-transformers";
 
 /* SERVER instance */
 const serverApi = axios.create({
+  // 'server-api' URL is rewritten in the Nginx conf to the correct URL
   baseURL: import.meta.env.PROD ? "server-api" : `${import.meta.env.VITE_SERVER_URL}/graphql`,
   headers: {
     "Content-Type": "application/json",
+    // SERVER-API-KEY header is set in the Nginx conf for other environments different from the local development
     "SERVER-API-KEY": import.meta.env.PROD ? undefined : import.meta.env.VITE_SERVER_API_KEY,
   },
 });
@@ -14,6 +16,7 @@ serverApi.interceptors.response.use(transformDateStringToDate);
 
 /* MAFILDB instance */
 const mafildbApi = axios.create({
+  // 'mafildb-api' URL is rewritten in the Nginx conf to the correct URL
   baseURL: import.meta.env.PROD ? "mafildb-api" : `${import.meta.env.VITE_MAFILDB_API_URL}/api`,
   headers: {
     "Content-Type": "application/json",
