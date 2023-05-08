877f7ed7a059c8636614e3bc22db43c8726dd28f
Lukáš Kratochvíl <lkratochvil57@gmail.com>
2023-05-08 09:49:06 +0200

    fix: corrected secrets file path

--- frontend/src/util/secrets.ts
+++ frontend/src/util/secrets.ts
@@ -5,7 +5,7 @@ type Secrets = {
 let secrets: Secrets;
 
 const fetchSecrets = async (): Promise<void> => {
-  const secretsFile = await fetch("assets/secrets.json");
+  const secretsFile = await fetch("secrets.json");
   secrets = (await secretsFile.json()) as Secrets;
 }
 
