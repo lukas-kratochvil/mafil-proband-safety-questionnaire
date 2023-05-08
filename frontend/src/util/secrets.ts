0dac802b7d1ae6303f68503d68f4e77a79c401ff
Lukáš Kratochvíl <lkratochvil57@gmail.com>
2023-05-08 11:16:21 +0200

    feat: setting correct HTTP headers to fetcj JSON file

--- frontend/src/util/secrets.ts
+++ frontend/src/util/secrets.ts
@@ -1,15 +1,19 @@
 type Secrets = {
   oidcClientId: string;
-}
+};
 
 let secrets: Secrets;
 
 const fetchSecrets = async (): Promise<void> => {
-  const secretsFile = await fetch("assets/secrets.json");
+  const headers = new Headers({
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  });
+  const secretsFile = await fetch("assets/secrets.json", { headers });
   console.log("Secrets file:");
   console.log(secretsFile);
   secrets = (await secretsFile.json()) as Secrets;
-}
+};
 
 fetchSecrets();
 
