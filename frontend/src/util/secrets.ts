3eba0932389259f56b495841872eea19ec90a0dc
Lukáš Kratochvíl <lkratochvil57@gmail.com>
2023-05-08 11:05:27 +0200

    feat: logging to the console if the secrets file is fetched

--- frontend/src/util/secrets.ts
+++ frontend/src/util/secrets.ts
@@ -5,7 +5,9 @@ type Secrets = {
 let secrets: Secrets;
 
 const fetchSecrets = async (): Promise<void> => {
-  const secretsFile = await fetch("secrets.json");
+  const secretsFile = await fetch("assets/secrets.json");
+  console.log("Secrets file:");
+  console.log(secretsFile);
   secrets = (await secretsFile.json()) as Secrets;
 }
 
