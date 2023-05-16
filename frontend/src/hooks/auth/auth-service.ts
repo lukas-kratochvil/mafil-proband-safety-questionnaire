d22e3c039ddbb012964f3f83d99e9184d5d4dca7
Lukáš Kratochvíl <lkratochvil57@gmail.com>
2023-05-17 00:46:54 +0200

    feat: added OIDC MFA - second factor authentication

--- frontend/src/hooks/auth/auth-service.ts
+++ frontend/src/hooks/auth/auth-service.ts
@@ -3,12 +3,16 @@ import { RoutingPath } from "@app/routing-paths";
 import { authenticateOperator } from "@app/util/server_API/calls";
 import { IOperatorDTO } from "@app/util/server_API/dto";
 
+// Multi-factor authentication - URL of the second factor authentication provider
+const MFA_URL = "https://refeds.org/profile/mfa";
+
 const config: UserManagerSettings = {
   authority: "https://oidc.muni.cz/oidc",
   client_id: "client_id", // TODO: add MUNI OIDC client ID
   redirect_uri: `${window.location.origin}${RoutingPath.WAITING_ROOM}`,
   scope: "openid profile email eduperson_entitlement",
   post_logout_redirect_uri: `${window.location.origin}${RoutingPath.LOGIN}`,
+  acr_values: MFA_URL,
 };
 
 export class AuthService {
@@ -38,6 +42,12 @@ export class AuthService {
     try {
       // TODO: specify optional URL or leave it undefined?
       const jpmUser = await this.userManager.signinRedirectCallback(RoutingPath.WAITING_ROOM);
+
+      // Check that the user used MFA to authenticate
+      if (jpmUser.profile.acr !== MFA_URL) {
+        return null;
+      }
+
       // Check that the user is registered in our app and should have access to the authenticated part of the app
       return await authenticateOperator({
         name: jpmUser.profile.given_name ?? "",
