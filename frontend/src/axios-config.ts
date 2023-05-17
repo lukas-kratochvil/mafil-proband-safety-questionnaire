b1b1bd2d8eac4ffbe7599815eb900272467fe187
Lukáš Kratochvíl <lkratochvil57@gmail.com>
2023-05-17 17:49:01 +0200

    fix: corrected comments in the frontend axios config

--- frontend/src/axios-config.ts
+++ frontend/src/axios-config.ts
@@ -8,7 +8,7 @@ const serverApi = axios.create({
   baseURL: import.meta.env.PROD ? "server-api" : `${import.meta.env.VITE_SERVER_URL}/graphql`,
   headers: {
     "Content-Type": "application/json",
-    // SERVER-API-KEY header is set in the Nginx conf for other environments different from the local development
+    // SERVER-API-KEY header is set in the Nginx conf for all the environments except the local development environment
     "SERVER-API-KEY": import.meta.env.PROD ? undefined : import.meta.env.VITE_SERVER_API_KEY,
   },
 });
@@ -17,8 +17,8 @@ serverApi.interceptors.response.use(transformDateStringToDate);
 
 /* MAFILDB instance */
 const mafildbApi = axios.create({
+  // we do not communicate with MAFILDB API when developing locally due to the OIDC authentication that cannot be done
   // 'mafildb-api' URL is rewritten in the Nginx conf to the correct URL
-  // we do not communicate with MAFILDB API when developing locally
   baseURL: import.meta.env.PROD ? "mafildb-api" : undefined,
   headers: {
     "Content-Type": "application/json",
@@ -29,7 +29,7 @@ mafildbApi.interceptors.request.use(async (config) => {
   const authService = AuthService.getInstance();
   const authUser = await authService.getAuthUser();
   if (authUser) {
-    // set Authorization header to user's OIDC access_token, so that a request to the MAFILDB API will proceed
+    // set user's OIDC access_token in the Authorization header, so that a request to the MAFILDB API will proceed
     config.headers.Authorization = `Bearer ${authUser.access_token}`; // eslint-disable-line no-param-reassign
   }
   return config;
