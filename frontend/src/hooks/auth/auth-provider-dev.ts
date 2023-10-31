fb4fafd2e9213c3fa4bd5ec01ccd70b6678beef6
Lukas Kratochvil <lkratochvil57@gmail.com>
2023-10-31 20:43:46 +0100

    feat: refactoring - extracted dev operator to the variable

--- frontend/src/hooks/auth/auth-provider-dev.ts
+++ frontend/src/hooks/auth/auth-provider-dev.ts
@@ -1,10 +1,17 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { IOperatorAuthorization } from "@app/model/auth";
 import { RoutingPath } from "@app/routing-paths";
 import { authenticateOperator } from "@app/util/server_API/calls";
 import { IAuth, Operator } from "./AuthProvider";
 
 const SESSION_STORAGE_OPERATOR_KEY = "operator";
+const DEV_OPERATOR: IOperatorAuthorization = {
+  name: "Jiří",
+  surname: "Svoboda",
+  username: "987654",
+  email: "987654@muni.cz",
+};
 
 export const useAuthProviderDev = (): IAuth => {
   const navigate = useNavigate();
@@ -15,12 +22,7 @@ export const useAuthProviderDev = (): IAuth => {
 
   const logIn = async (): Promise<void> => {
     try {
-      const validOperator = await authenticateOperator({
-        name: "Jiří",
-        surname: "Svoboda",
-        username: "987654",
-        email: "987654@muni.cz",
-      });
+      const validOperator = await authenticateOperator(DEV_OPERATOR);
       setOperator(validOperator);
       window.sessionStorage.setItem(SESSION_STORAGE_OPERATOR_KEY, JSON.stringify(validOperator));
       navigate(RoutingPath.WAITING_ROOM);
