6c8435482c6c68b34b79c4a633908b9d738f4af8
Lukas Kratochvil <lkratochvil57@gmail.com>
2023-12-14 14:14:42 +0100

    feat: added IntelliSense for Vite env variables

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,1 +1,21 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  // used in PROD and DEV environment
+  readonly VITE_APP_BAR_COLOR: string;
+
+  // used only in PROD environment
+  readonly VITE_JPM_CLIENT_ID: string;
+
+  // used only in DEV environment
+  readonly VITE_SERVER_URL: string;
+  readonly VITE_WEB_API_KEY: string;
+  readonly VITE_OPERATOR_FIRSTNAME: string;
+  readonly VITE_OPERATOR_SURNAME: string;
+  readonly VITE_OPERATOR_USERNAME: string;
+  readonly VITE_OPERATOR_EMAIL: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
