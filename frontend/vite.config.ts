a8570504dbea95592d7614885eebecfe2538cdb8
Lukas Kratochvil <lkratochvil57@gmail.com>
2024-03-22 23:14:51 +0100

    feat: refactoring Vite config - removed unnecessary path alias

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,7 +2,6 @@
 
 // eslint-disable-next-line import/no-extraneous-dependencies
 import react from "@vitejs/plugin-react";
-import * as path from "path";
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
@@ -15,7 +14,7 @@ export default defineConfig({
       // must set this to false because of the error occurring while testing: "@vitejs/plugin-react can't detect preamble. Something is wrong. See https://github.com/vitejs/vite-plugin-react/pull/11#discussion_r430879201"
       fastRefresh: false,
     }),
-    tsconfigPaths(), // gives Vite the ability to resolve imports using TypeScript's path mapping from tsconfig
+    tsconfigPaths(), // gives Vite the ability to resolve imports using TypeScript's path mapping from tsconfig (https://vitest.dev/guide/common-errors)
   ],
   server: {
     host: true, // it's a must for Docker container port mapping to work
@@ -23,12 +22,6 @@ export default defineConfig({
     port: +process.env.PORT,
   },
   test: {
-    alias: [
-      {
-        find: "@test-utils",
-        replacement: path.resolve(__dirname, "./src/__tests__/utils.ts"),
-      },
-    ],
     clearMocks: true,
     environment: "jsdom",
     globals: true,
