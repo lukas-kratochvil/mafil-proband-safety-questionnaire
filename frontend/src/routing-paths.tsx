ffaac2d8adbb8e75a39493da283a53f2eb3b45d5
Lukas Kratochvil <lkratochvil57@gmail.com>
2024-03-27 18:05:01 +0100

    feat: refactoring - instead of an enum using type and const object for the RoutingPath

--- frontend/src/routing-paths.tsx
+++ frontend/src/routing-paths.tsx
@@ -1,16 +1,20 @@
-export enum RoutingPath {
-  PROBAND_HOME = "/home",
-  PROBAND_FORM = "/form",
-  LOGIN = "/login",
-  OIDC_LOGIN = "/oidc-login",
-  LOGOUT = "/logout",
-  AUTH = "/auth",
-  PHANTOM_FORM = `${AUTH}/phantom-form`,
-  WAITING_ROOM = `${AUTH}/waiting-room`,
-  WAITING_ROOM_FORM = `${WAITING_ROOM}/form`,
-  APPROVAL_ROOM = `${AUTH}/approval-room`,
-  APPROVAL_ROOM_FORM = `${APPROVAL_ROOM}/form`,
-  RECENT_VISITS = `${AUTH}/recent-visits`,
-  RECENT_VISITS_DUPLICATE = `${RECENT_VISITS}/duplicate`,
-  RECENT_VISITS_VISIT = `${RECENT_VISITS}/visit`,
-}
+import type { ObjectValues } from "./util/utils";
+
+export const RoutingPath = {
+  PROBAND_HOME: "/home",
+  PROBAND_FORM: "/form",
+  LOGIN: "/login",
+  OIDC_LOGIN: "/oidc-login",
+  LOGOUT: "/logout",
+  AUTH: "/auth",
+  PHANTOM_FORM: "/auth/phantom-form",
+  WAITING_ROOM: "/auth/waiting-room",
+  WAITING_ROOM_FORM: "/auth/waiting-room/form",
+  APPROVAL_ROOM: "/auth/approval-room",
+  APPROVAL_ROOM_FORM: "/auth/approval-room/form",
+  RECENT_VISITS: "/auth/recent-visits",
+  RECENT_VISITS_DUPLICATE: "/auth/recent-visits/duplicate",
+  RECENT_VISITS_VISIT: "/auth/recent-visits/visit",
+} as const;
+
+export type RoutingPath = ObjectValues<typeof RoutingPath>;
