54dff65cb30e2e4a07f676e9e00dbf9afdb60424
Lukas Kratochvil <lkratochvil57@gmail.com>
2024-04-19 17:08:18 +0200

    fix: env var substitution - processing each JS file line-by-line

--- frontend/start-nginx.sh
+++ frontend/start-nginx.sh
@@ -9,8 +9,18 @@ export INJECT_VARS=$(printenv | grep '^INJECT_' | sed 's/^INJECT_//' | awk '{pri
 echo "$INJECT_VARS"
 
 # loop through each JavaScript file in the production folder and replace any $VARIABLE with the actual value of that environment variable
-for file in $JSFOLDER; do
-  cat $file | envsubst $INJECT_VARS | tee $file
+for file in $JSFOLDER/*; do
+  # create a temporary file to store the modified lines
+  temp_file=$(mktemp)
+
+  # read each line of the file
+  while IFS= read -r line; do
+    # substitute environment variables in the line and append it to the temporary file
+    echo "$line" | envsubst "$INJECT_VARS" >> "$temp_file"
+  done < "$file"
+
+  # replace the original file with the modified temporary file
+  mv "$temp_file" "$file"
 done
 
 nginx -g 'daemon off;'
