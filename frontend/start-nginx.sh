812648b9609cf53b02a426e4d5ad0706f1989458
Lukas Kratochvil <lkratochvil57@gmail.com>
2024-04-19 12:35:46 +0200

    fix: start-nginx.sh substitutes only env vars prefixed with 'INJECT_'

--- frontend/start-nginx.sh
+++ frontend/start-nginx.sh
@@ -4,12 +4,13 @@
 # This script injects environment variables in frontend JavaScript files and then starts the Nginx server.
 #-----------------------------------------------------------------------------------------------------------------------
 
-# get names of all existing environment variables and store them in the $EXISTING_VARS
-export EXISTING_VARS=$(printenv | awk -F= '{print $1}' | sed 's/^/\$/g' | paste -sd,);
+# get names of all environment variables that should be injected into Nginx JavaScript files (prefixed with 'INJECT_') and store them in the $INJECT_VARS
+export INJECT_VARS=$(printenv | grep '^INJECT_' | sed 's/^INJECT_//' | awk '{print "$" $1}' | paste -sd,);
+echo "$INJECT_VARS"
 
 # loop through each JavaScript file in the production folder and replace any $VARIABLE with the actual value of that environment variable
 for file in $JSFOLDER; do
-  cat $file | envsubst $EXISTING_VARS | tee $file
+  cat $file | envsubst $INJECT_VARS | tee $file
 done
 
 nginx -g 'daemon off;'
