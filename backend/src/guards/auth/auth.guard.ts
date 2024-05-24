acf501a6a05ab6ac98160032047aaba36f48839b
Lukas Kratochvil <lkratochvil57@gmail.com>
2024-05-25 01:02:03 +0200

    feat: refactoring AuthGuard and log messages

--- backend/src/guards/auth/auth.guard.ts
+++ backend/src/guards/auth/auth.guard.ts
@@ -1,3 +1,4 @@
+import type { Request } from "express";
 import { CanActivate, ExecutionContext, Injectable, Logger } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
 import { Reflector } from "@nestjs/core";
@@ -18,17 +19,16 @@ export class AuthGuard implements CanActivate {
 
     const gqlExContext = GqlExecutionContext.create(exContext);
     const gqlContext = gqlExContext.getContext();
-    const request = gqlContext.req;
+    const request = gqlContext.req as Request;
+
+    // check if issued API key is valid, other services will be denied access
     // api key HTTP header name must be in the lower case
     const apiKey = request.headers["reg-api-key"] ?? "";
-
-    // Checking web service API key. Other services will be denied access.
-    if (apiKey === this.config.get("WEB_API_KEY", { infer: true })) {
-      return true;
+    if (apiKey !== this.config.get("WEB_API_KEY", { infer: true })) {
+      this.logger.error(`Request from origin '${request.headers.origin}' [${request.headers["user-agent"]}] has invalid API key: '${apiKey}'!`);
+      return false;
     }
 
-    const ip = request.ip ?? "";
-    this.logger.error(`Request from '${ip}' has invalid API key: '${apiKey}'!`);
-    return false;
+    return true;
   }
 }
