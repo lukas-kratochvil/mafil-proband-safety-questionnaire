1d253c745721a629ca45ea274c98bd79285345a9
Lukas Kratochvil <lkratochvil57@gmail.com>
2024-05-24 17:54:56 +0200

    feat: logging Prisma 'error' and 'warn' events

--- backend/src/prisma/prisma.service.ts
+++ backend/src/prisma/prisma.service.ts
@@ -1,9 +1,24 @@
-import { Injectable, OnModuleInit } from "@nestjs/common";
+import { Injectable, Logger, OnModuleInit } from "@nestjs/common";
 import { PrismaClient } from "@prisma/client";
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
+export class PrismaService
+  extends PrismaClient<{
+    log: [{ level: "warn"; emit: "event" }, { level: "error"; emit: "event" }];
+  }>
+  implements OnModuleInit
+{
+  private readonly logger = new Logger(PrismaService.name);
+
   async onModuleInit() {
+    // Setup logging
+    this.$on("warn", (event) => {
+      this.logger.warn(event.message, event.target);
+    });
+    this.$on("error", (event) => {
+      this.logger.error(event.message, event.target);
+    });
+
     await this.$connect();
   }
 }
