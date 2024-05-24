6ed9bcb64e31b3720852fbd9a7e9ff0a3452de21
Lukas Kratochvil <lkratochvil57@gmail.com>
2024-05-24 17:03:41 +0200

    feat: added HTTP header Accept to accept only JSON data from our server

--- frontend/src/util/axios/serverApi.ts
+++ frontend/src/util/axios/serverApi.ts
@@ -8,6 +8,7 @@ export const serverApi = axios.create({
   // 'server-api' URL is rewritten in the Nginx conf to the correct URL
   baseURL: import.meta.env.PROD ? "server-api" : `${import.meta.env.VITE_SERVER_URL}/graphql`,
   headers: {
+    Accept: "application/json",
     "Content-Type": "application/json",
     // REG-API-KEY header is set in the Nginx conf for all the environments except the local development environment
     "REG-API-KEY": import.meta.env.PROD ? undefined : import.meta.env.VITE_REG_APP_API_KEY,
