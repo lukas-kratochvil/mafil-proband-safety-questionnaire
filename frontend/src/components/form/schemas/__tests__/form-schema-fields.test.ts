37b2c3bb5207d0b9fb0068626150b7ecd294752e
Lukas Kratochvil <lkratochvil57@gmail.com>
2024-07-05 21:55:36 +0200

    feat: frontend form schema tests - refactoring

--- frontend/src/components/form/schemas/__tests__/form-schema-fields.test.ts
+++ frontend/src/components/form/schemas/__tests__/form-schema-fields.test.ts
@@ -4,114 +4,152 @@ import { probandFormSchema } from "../proband-form-schema";
 //----------------------------------------------------------------------
 // Tests
 //----------------------------------------------------------------------
-describe("form schemas", () => {
-  describe("proband form schema", () => {
-    test.each([
-      ["John", true],
-      [" John ", true],
-    ])("name: '%s' -> %s", (input: string, isValid: boolean) => {
-      const schema = probandFormSchema.pick(["name"]) as AnySchema;
-      const toValidate = { name: input };
+describe("form schema", () => {
+  test.each([
+    ["John", true],
+    [" John ", true],
+  ])("name: '%s' -> %s", (input: string, expectedIsValid: boolean) => {
+    // ARRANGE
+    const schema = probandFormSchema.pick(["name"]) as AnySchema;
+    const toValidate = { name: input };
 
-      expect(schema.isValidSync(toValidate)).toBe(isValid);
-    });
+    // ACT
+    const isValid = schema.isValidSync(toValidate);
 
-    test.each([
-      ["Wick", true],
-      [" Wick ", true],
-    ])("surname: '%s' -> %s", (input: string, isValid: boolean) => {
-      const schema = probandFormSchema.pick(["surname"]) as AnySchema;
-      const toValidate = { surname: input };
+    // ASSERT
+    expect(isValid).toBe(expectedIsValid);
+  });
 
-      expect(schema.isValidSync(toValidate)).toBe(isValid);
-    });
+  test.each([
+    ["Wick", true],
+    [" Wick ", true],
+  ])("surname: '%s' -> %s", (input: string, expectedIsValid: boolean) => {
+    // ARRANGE
+    const schema = probandFormSchema.pick(["surname"]) as AnySchema;
+    const toValidate = { surname: input };
 
-    test.each([
-      ["0025644861", true],
-      ["   0025644861", true],
-      ["0025644861   ", true],
-      [" 0025644861 ", true],
-    ])("personal ID: '%s' -> %s", (input: string, isValid: boolean) => {
-      const schema = probandFormSchema.pick(["personalId"]) as AnySchema;
-      const toValidate = { personalId: input };
+    // ACT
+    const isValid = schema.isValidSync(toValidate);
 
-      expect(schema.isValidSync(toValidate)).toBe(isValid);
-    });
+    // ASSERT
+    expect(isValid).toBe(expectedIsValid);
+  });
 
-    test.each([
-      ["2007-12-03T10:15:30Z", true],
-      ["9999-12-03T10:15:30Z", false],
-    ])("birthdate: '%s' -> %s", (input: string, isValid: boolean) => {
-      const schema = probandFormSchema.pick(["birthdate"]) as AnySchema;
-      const toValidate = { birthdate: input };
+  test.each([
+    ["0025644861", true],
+    ["   0025644861", true],
+    ["0025644861   ", true],
+    [" 0025644861 ", true],
+  ])("personal ID: '%s' -> %s", (input: string, expectedIsValid: boolean) => {
+    // ARRANGE
+    const schema = probandFormSchema.pick(["personalId"]) as AnySchema;
+    const toValidate = { personalId: input };
 
-      expect(schema.isValidSync(toValidate)).toBe(isValid);
-    });
+    // ACT
+    const isValid = schema.isValidSync(toValidate);
 
-    test.each([
-      ["-1", false],
-      ["0", false],
-      ["1", true],
-      [" 1 ", true],
-      ["0,5", false],
-      ["0.5", false],
-    ])("height cm: '%s' -> %s", (input: string, isValid: boolean) => {
-      const schema = probandFormSchema.pick(["heightCm"]) as AnySchema;
-      const toValidate = { heightCm: input };
+    // ASSERT
+    expect(isValid).toBe(expectedIsValid);
+  });
 
-      expect(schema.isValidSync(toValidate)).toBe(isValid);
-    });
+  test.each([
+    ["2007-12-03T10:15:30Z", true],
+    ["9999-12-03T10:15:30Z", false],
+  ])("birthdate: '%s' -> %s", (input: string, expectedIsValid: boolean) => {
+    // ARRANGE
+    const schema = probandFormSchema.pick(["birthdate"]) as AnySchema;
+    const toValidate = { birthdate: input };
 
-    test.each([
-      ["-1", false],
-      ["0", false],
-      ["1", true],
-      [" 1 ", true],
-      ["0,5", false],
-      ["0.5", false],
-    ])("weight kg: '%s' -> %s", (input: string, isValid: boolean) => {
-      const schema = probandFormSchema.pick(["weightKg"]) as AnySchema;
-      const toValidate = { weightKg: input };
+    // ACT
+    const isValid = schema.isValidSync(toValidate);
 
-      expect(schema.isValidSync(toValidate)).toBe(isValid);
-    });
+    // ASSERT
+    expect(isValid).toBe(expectedIsValid);
+  });
 
-    test.each([
-      ["", false],
-      ["email", false],
-      ["name.surname@email", false],
-      ["name.surname@email.", false],
-      ["name.surname@.c", false],
-      ["name.surname@e.c", false],
-      ["name.surname@email.c", false],
-      ["name.surname@email.com.c", false],
-      ["name.surname@email.com..com", false],
-      ["name.surname@email.com", true],
-      [" name.surname@email.com ", true],
-    ])("email: '%s' -> %s", (input: string, isValid: boolean) => {
-      const schema = probandFormSchema.pick(["email"]) as AnySchema;
-      const toValidate = { email: input };
+  test.each([
+    ["-1", false],
+    ["0", false],
+    ["1", true],
+    [" 1 ", true],
+    ["0,5", false],
+    ["0.5", false],
+  ])("height cm: '%s' -> %s", (input: string, expectedIsValid: boolean) => {
+    // ARRANGE
+    const schema = probandFormSchema.pick(["heightCm"]) as AnySchema;
+    const toValidate = { heightCm: input };
 
-      expect(schema.isValidSync(toValidate)).toBe(isValid);
-    });
+    // ACT
+    const isValid = schema.isValidSync(toValidate);
 
-    test.each([
-      ["123456789", true], // without any national code prefix
-      ["00155625", true],
-      ["00 1 556 25", true],
-      [" 00 1 556 25 ", true],
-      ["+420155625", true],
-      ["+420 1 556 25", true],
-      [" +420 1 556 25 ", true],
-      ["+420", false],
-      ["+420 5", true],
-      ["00", false],
-      ["00 5", false],
-    ])("phone: '%s' -> %s", (input: string, isValid: boolean) => {
-      const schema = probandFormSchema.pick(["phone"]) as AnySchema;
-      const toValidate = { phone: input };
+    // ASSERT
+    expect(isValid).toBe(expectedIsValid);
+  });
 
-      expect(schema.isValidSync(toValidate)).toBe(isValid);
-    });
+  test.each([
+    ["-1", false],
+    ["0", false],
+    ["1", true],
+    [" 1 ", true],
+    ["0,5", false],
+    ["0.5", false],
+  ])("weight kg: '%s' -> %s", (input: string, expectedIsValid: boolean) => {
+    // ARRANGE
+    const schema = probandFormSchema.pick(["weightKg"]) as AnySchema;
+    const toValidate = { weightKg: input };
+
+    // ACT
+    const isValid = schema.isValidSync(toValidate);
+
+    // ASSERT
+    expect(isValid).toBe(expectedIsValid);
+  });
+
+  test.each([
+    ["", false],
+    ["email", false],
+    ["name.surname@email", false],
+    ["name.surname@email.", false],
+    ["name.surname@.c", false],
+    ["name.surname@e.c", false],
+    ["name.surname@email.c", false],
+    ["name.surname@email.com.c", false],
+    ["name.surname@email.com..com", false],
+    ["name.surname@email.com", true],
+    [" name.surname@email.com ", true],
+  ])("email: '%s' -> %s", (input: string, expectedIsValid: boolean) => {
+    // ARRANGE
+    const schema = probandFormSchema.pick(["email"]) as AnySchema;
+    const toValidate = { email: input };
+
+    // ACT
+    const isValid = schema.isValidSync(toValidate);
+
+    // ASSERT
+    expect(isValid).toBe(expectedIsValid);
+  });
+
+  test.each([
+    ["123456789", true], // without any national code prefix
+    ["00155625", true],
+    ["00 1 556 25", true],
+    [" 00 1 556 25 ", true],
+    ["+420155625", true],
+    ["+420 1 556 25", true],
+    [" +420 1 556 25 ", true],
+    ["+420", false],
+    ["+420 5", true],
+    ["00", false],
+    ["00 5", false],
+  ])("phone: '%s' -> %s", (input: string, expectedIsValid: boolean) => {
+    // ARRANGE
+    const schema = probandFormSchema.pick(["phone"]) as AnySchema;
+    const toValidate = { phone: input };
+
+    // ACT
+    const isValid = schema.isValidSync(toValidate);
+
+    // ASSERT
+    expect(isValid).toBe(expectedIsValid);
   });
 });
