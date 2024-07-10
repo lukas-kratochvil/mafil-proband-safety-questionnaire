8f2ca06165a05315dfa50b65cfc1411b81c5411a
Lukas Kratochvil <lkratochvil57@gmail.com>
2024-07-10 09:25:27 +0200

    feat: created tests for QuestionTranslationService

--- backend/src/api/question-translation/question-translation.service.spec.ts
+++ backend/src/api/question-translation/question-translation.service.spec.ts
@@ -1,9 +1,27 @@
 import { Test } from "@nestjs/testing";
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient, type QuestionTranslation } from "@prisma/client";
 import { DeepMockProxy, mockDeep } from "jest-mock-extended";
 import { PrismaService } from "@app/prisma/prisma.service";
+import type { CreateQuestionTranslationInput } from "./dto/create-question-translation.input";
+import type { UpdateQuestionTranslationInput } from "./dto/update-question-translation.input";
 import { QuestionTranslationService } from "./question-translation.service";
 
+//----------------------------------------------------------------------
+// Test data
+//----------------------------------------------------------------------
+const questionTranslation: QuestionTranslation = {
+  id: "1",
+  languageId: "1",
+  questionId: "1",
+  text: "test",
+  createdAt: new Date(),
+  updatedAt: new Date(),
+  deletedAt: null,
+};
+
+//----------------------------------------------------------------------
+// Tests
+//----------------------------------------------------------------------
 describe("QuestionTranslationService", () => {
   let questionTranslationService: QuestionTranslationService;
   let prisma: DeepMockProxy<PrismaClient>;
@@ -20,7 +38,30 @@ describe("QuestionTranslationService", () => {
     prisma = module.get<PrismaService, DeepMockProxy<PrismaClient>>(PrismaService);
   });
 
-  it("should be defined", () => {
-    expect(questionTranslationService).toBeDefined();
+  it("create question translation", () => {
+    // ARRANGE
+    const questionTranslationInput: CreateQuestionTranslationInput = { ...questionTranslation };
+    prisma.questionTranslation.create.mockResolvedValueOnce(questionTranslation);
+
+    // ACT
+    const createdQuestionTranslation = questionTranslationService.create(questionTranslationInput);
+
+    // ASSERT
+    expect(createdQuestionTranslation).resolves.toStrictEqual(questionTranslation);
+  });
+
+  it("update question translation", () => {
+    // ARRANGE
+    const questionTranslationInput: UpdateQuestionTranslationInput = { ...questionTranslation, text: "test2" };
+    prisma.questionTranslation.update.mockResolvedValueOnce({ ...questionTranslation, ...questionTranslationInput });
+
+    // ACT
+    const updatedQuestionTranslation = questionTranslationService.update(
+      questionTranslationInput.id,
+      questionTranslationInput
+    );
+
+    // ASSERT
+    expect(updatedQuestionTranslation).resolves.toStrictEqual({ ...questionTranslation, ...questionTranslationInput });
   });
 });
