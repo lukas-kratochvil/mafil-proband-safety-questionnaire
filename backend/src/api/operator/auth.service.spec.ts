21326538aac8d53bdf038bfc2444e88b904eb967
Lukas Kratochvil <lkratochvil57@gmail.com>
2024-07-11 18:11:08 +0200

    feat: created tests for AuthService

--- backend/src/api/operator/auth.service.spec.ts
+++ backend/src/api/operator/auth.service.spec.ts
@@ -1,10 +1,31 @@
+import { UnauthorizedException } from "@nestjs/common";
 import { Test, TestingModule } from "@nestjs/testing";
-import type { PrismaClient } from "@prisma/client";
+import { OperatorRole, type Operator, type PrismaClient } from "@prisma/client";
 import { mockDeep, type DeepMockProxy } from "jest-mock-extended";
 import { PrismaService } from "@app/prisma/prisma.service";
 import { AuthService } from "./auth.service";
 import { AUTH_PRISMA_SERVICE } from "./constants";
 
+//----------------------------------------------------------------------
+// Test data
+//----------------------------------------------------------------------
+const operator: Operator = {
+  id: "1",
+  name: "John",
+  surname: "Smith",
+  username: "john.smith@mail.com",
+  email: "john.smith@mail.com",
+  role: OperatorRole.MR,
+  isValid: true,
+  lastLoggedAt: null,
+  createdAt: new Date(),
+  updatedAt: new Date(),
+  deletedAt: null,
+};
+
+//----------------------------------------------------------------------
+// Tests
+//----------------------------------------------------------------------
 describe("AuthService", () => {
   let authService: AuthService;
   let prisma: DeepMockProxy<PrismaClient>;
@@ -15,7 +36,7 @@ describe("AuthService", () => {
         AuthService,
         {
           provide: AUTH_PRISMA_SERVICE,
-          useClass: AuthService,
+          useClass: PrismaService,
         },
       ],
     })
@@ -27,7 +48,76 @@ describe("AuthService", () => {
     prisma = module.get<PrismaService, DeepMockProxy<PrismaClient>>(AUTH_PRISMA_SERVICE);
   });
 
-  it("should be defined", () => {
-    expect(authService).toBeDefined();
+  it("authenticate operator", () => {
+    // ARRANGE
+    const lastLoggedAt = new Date();
+    prisma.operator.findUniqueOrThrow.mockResolvedValueOnce(operator);
+    prisma.operator.update.mockResolvedValueOnce({ ...operator, lastLoggedAt });
+
+    // ACT
+    const foundOperator = authService.authenticate(operator.name, operator.surname, operator.username, operator.email);
+
+    // ASSERT
+    expect(foundOperator).resolves.toStrictEqual({ ...operator, lastLoggedAt });
+  });
+
+  it("authenticate operator - operator not found", () => {
+    // ARRANGE
+    prisma.operator.findUniqueOrThrow.mockRejectedValueOnce(new Error());
+
+    // ACT
+    const foundOperator = authService.authenticate(operator.name, operator.surname, operator.username, operator.email);
+
+    // ASSERT
+    expect(foundOperator).rejects.toThrow(Error);
+  });
+
+  it("authenticate operator - operator not valid", () => {
+    // ARRANGE
+    const invalidOperator = {
+      ...operator,
+      isValid: false,
+    };
+    prisma.operator.findUniqueOrThrow.mockResolvedValueOnce(invalidOperator);
+
+    // ACT
+    const foundOperator = authService.authenticate(operator.name, operator.surname, operator.username, operator.email);
+
+    // ASSERT
+    expect(foundOperator).rejects.toThrow(UnauthorizedException);
+  });
+
+  it("authenticate operator - changed operator data", () => {
+    // ARRANGE
+    const changedOperator = {
+      ...operator,
+      name: `${operator.name}_X`,
+      surname: `${operator.surname}_X`,
+      email: `${operator.email}_X`,
+    };
+    prisma.operator.findUniqueOrThrow.mockResolvedValueOnce(operator);
+    prisma.operator.update.mockResolvedValueOnce(changedOperator);
+
+    // ACT
+    const foundOperator = authService.authenticate(
+      changedOperator.name,
+      changedOperator.surname,
+      changedOperator.username,
+      changedOperator.email
+    );
+
+    // ASSERT
+    expect(foundOperator).resolves.toStrictEqual(changedOperator);
+  });
+
+  it("verify operator", () => {
+    // ARRANGE
+    prisma.operator.findUniqueOrThrow.mockResolvedValueOnce(operator);
+
+    // ACT
+    const foundOperator = authService.verify(operator.username);
+
+    // ASSERT
+    expect(foundOperator).resolves.toStrictEqual(operator);
   });
 });
