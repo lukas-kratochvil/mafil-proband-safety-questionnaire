63efb846f39e115c27c41c4b736ad972e14f7475
Lukas Kratochvil <lkratochvil57@gmail.com>
2024-07-12 12:45:40 +0200

    feat: refactoring - renamend MAFILDB generic response types

--- frontend/src/util/mafildb_API/response-types.ts
+++ frontend/src/util/mafildb_API/response-types.ts
@@ -21,35 +21,35 @@ type MDB_GetManySuccessResponse<T> = {
 };
 
 // Generic MAFILDB API response types
-type MDB_GetManyResponse<T> = MDB_GetManySuccessResponse<T> | MDB_ErrorResponse;
-type MDB_GetOneResponse<T> = T | MDB_ErrorResponse;
+type MDB_GetResponse<T> = T | MDB_ErrorResponse;
+type MDB_GetResponseComplex<T> = MDB_GetManySuccessResponse<T> | MDB_ErrorResponse;
 
-export type MDB_GetLanguagesResponse = MDB_GetManyResponse<MDB_LanguageDTO>;
+export type MDB_GetLanguagesResponse = MDB_GetResponseComplex<MDB_LanguageDTO>;
 
-export type MDB_GetLanguageResponse = MDB_GetOneResponse<MDB_LanguageDTO>;
+export type MDB_GetLanguageResponse = MDB_GetResponse<MDB_LanguageDTO>;
 
-export type MDB_GetProjectsResponse = MDB_GetManyResponse<MDB_ProjectDTO>;
+export type MDB_GetProjectsResponse = MDB_GetResponseComplex<MDB_ProjectDTO>;
 
-export type MDB_GetProjectResponse = MDB_GetOneResponse<MDB_ProjectDTO>;
+export type MDB_GetProjectResponse = MDB_GetResponse<MDB_ProjectDTO>;
 
-export type MDB_GetDevicesResponse = MDB_GetManyResponse<MDB_DeviceDTO>;
+export type MDB_GetDevicesResponse = MDB_GetResponseComplex<MDB_DeviceDTO>;
 
-export type MDB_GetDeviceResponse = MDB_GetOneResponse<MDB_DeviceDTO>;
+export type MDB_GetDeviceResponse = MDB_GetResponse<MDB_DeviceDTO>;
 
-export type MDB_CreateSubjectResponse = MDB_GetOneResponse<MDB_SubjectDTO>;
+export type MDB_CreateSubjectResponse = MDB_GetResponse<MDB_SubjectDTO>;
 
-export type MDB_CreateVisitResponse = MDB_GetOneResponse<MDB_VisitDTO>;
+export type MDB_CreateVisitResponse = MDB_GetResponse<MDB_VisitDTO>;
 
-export type MDB_UpdateVisitSignatureStateResponse = MDB_GetOneResponse<
+export type MDB_UpdateVisitSignatureStateResponse = MDB_GetResponse<
   Pick<MDB_VisitDTO, "registration_signature_status">
 >;
 
-export type MDB_GetVisitsResponse = MDB_GetManyResponse<MDB_VisitDTO>;
+export type MDB_GetVisitsResponse = MDB_GetResponseComplex<MDB_VisitDTO>;
 
-export type MDB_GetVisitResponse = MDB_GetOneResponse<MDB_VisitDTO>;
+export type MDB_GetVisitResponse = MDB_GetResponse<MDB_VisitDTO>;
 
-export type MDB_AddPdfToVisitResponse = MDB_GetOneResponse<
+export type MDB_AddPdfToVisitResponse = MDB_GetResponse<
   Pick<MDB_VisitFileDTO, "id" | "uploaded" | "name" | "file_type" | "mime_type">
 >;
 
-export type MDB_GetVisitFilesResponse = MDB_GetOneResponse<MDB_VisitFileDTO[]>;
+export type MDB_GetVisitFilesResponse = MDB_GetResponse<MDB_VisitFileDTO[]>;
