da18619b299cf02487e8c37b02bd9c6f35ed8429
Lukas Kratochvil <lkratochvil57@gmail.com>
2024-07-16 14:10:31 +0200

    fix: Vite build - ignoring 'use client' warning since we are not using SSR

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -9,6 +9,17 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // Vite: https://vitejs.dev/config/
 // Vitest: https://vitest.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      // ignore "use client" warning since we are not using SSR: https://github.com/TanStack/query/pull/5161#issuecomment-1477389761
+      onwarn(warning, warn) {
+        if (warning.code === "MODULE_LEVEL_DIRECTIVE") {
+          return;
+        }
+        warn(warning);
+      },
+    },
+  },
   plugins: [
     react({
       // must set this to false because of the error occurring while testing: "@vitejs/plugin-react can't detect preamble. Something is wrong. See https://github.com/vitejs/vite-plugin-react/pull/11#discussion_r430879201"
