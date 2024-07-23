ded3fdfa58ec52018d2dddc33f6d179000e4cc1d
Lukas Kratochvil <lkratochvil57@gmail.com>
2024-07-23 22:48:43 +0200

    feat: created tests for HandednessTranslationResolver

--- backend/src/api/handedness-translation/handedness-translation.resolver.spec.ts
+++ backend/src/api/handedness-translation/handedness-translation.resolver.spec.ts
@@ -1,27 +1,80 @@
 import { Test } from "@nestjs/testing";
-import { PrismaClient } from "@prisma/client";
+import type { HandednessTranslation } from "@prisma/client";
 import { DeepMockProxy, mockDeep } from "jest-mock-extended";
 import { PrismaService } from "@app/prisma/prisma.service";
+import type { CreateHandednessTranslationInput } from "./dto/create-handedness-translation.input";
+import type { UpdateHandednessTranslationInput } from "./dto/update-handedness-translation.input";
 import { HandednessTranslationResolver } from "./handedness-translation.resolver";
 import { HandednessTranslationService } from "./handedness-translation.service";
 
+//----------------------------------------------------------------------
+// Test data
+//----------------------------------------------------------------------
+const handednessTranslation: HandednessTranslation = {
+  id: "1",
+  handednessId: "1",
+  languageId: "1",
+  text: "Right-handed",
+  createdAt: new Date(),
+  updatedAt: new Date(),
+  deletedAt: null,
+};
+
+//----------------------------------------------------------------------
+// Tests
+//----------------------------------------------------------------------
 describe("HandednessTranslationResolver", () => {
   let handednessTranslationResolver: HandednessTranslationResolver;
-  let prisma: DeepMockProxy<PrismaClient>;
+  let handednessTranslationService: DeepMockProxy<HandednessTranslationService>;
 
   beforeEach(async () => {
     const module = await Test.createTestingModule({
       providers: [HandednessTranslationResolver, HandednessTranslationService, PrismaService],
     })
-      .overrideProvider(PrismaService)
-      .useValue(mockDeep<PrismaClient>())
+      .overrideProvider(HandednessTranslationService)
+      .useValue(mockDeep<HandednessTranslationService>())
       .compile();
 
     handednessTranslationResolver = module.get<HandednessTranslationResolver>(HandednessTranslationResolver);
-    prisma = module.get<PrismaService, DeepMockProxy<PrismaClient>>(PrismaService);
+    handednessTranslationService = module.get<
+      HandednessTranslationService,
+      DeepMockProxy<HandednessTranslationService>
+    >(HandednessTranslationService);
   });
 
-  it("should be defined", () => {
-    expect(handednessTranslationResolver).toBeDefined();
+  it("create handedness translation", () => {
+    // ARRANGE
+    const createHandednessInput: CreateHandednessTranslationInput = { ...handednessTranslation };
+    handednessTranslationService.create.mockResolvedValueOnce(handednessTranslation);
+
+    // ACT
+    const createdHandednessTranslation
+      = handednessTranslationResolver.createHandednessTranslation(createHandednessInput);
+
+    // ASSERT
+    expect(createdHandednessTranslation).resolves.toEqual(handednessTranslation);
+  });
+
+  it("update handedness translation", () => {
+    // ARRANGE
+    const updateHandednessTranslationInput: UpdateHandednessTranslationInput = {
+      ...handednessTranslation,
+      text: "Left-handed",
+    };
+    handednessTranslationService.update.mockResolvedValueOnce({
+      ...handednessTranslation,
+      text: updateHandednessTranslationInput.text,
+    });
+
+    // ACT
+    const updatedHandednessTranslation = handednessTranslationResolver.updateHandednessTranslation(
+      updateHandednessTranslationInput
+    );
+
+    // ASSERT
+    expect(updatedHandednessTranslation).resolves.toEqual({
+      ...handednessTranslation,
+      text: updateHandednessTranslationInput.text,
+    });
   });
 });
