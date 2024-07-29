a186abbf82ba186ef51019ae48529cde087df802
Lukas Kratochvil <lkratochvil57@gmail.com>
2024-07-29 23:27:36 +0200

    feat: created tests for LanguageResolver

--- backend/src/api/language/language.resolver.spec.ts
+++ backend/src/api/language/language.resolver.spec.ts
@@ -1,27 +1,101 @@
 import { Test } from "@nestjs/testing";
-import { PrismaClient } from "@prisma/client";
+import type { Language } from "@prisma/client";
 import { DeepMockProxy, mockDeep } from "jest-mock-extended";
-import { PrismaService } from "@app/prisma/prisma.service";
+import type { CreateLanguageInput } from "./dto/create-language.input";
+import type { UpdateLanguageInput } from "./dto/update-language.input";
 import { LanguageResolver } from "./language.resolver";
 import { LanguageService } from "./language.service";
 
+//----------------------------------------------------------------------
+// Test data
+//----------------------------------------------------------------------
+const language: Language = {
+  id: "1",
+  code: "en",
+  name: "English",
+  createdAt: new Date(),
+  updatedAt: new Date(),
+  deletedAt: null,
+};
+
+//----------------------------------------------------------------------
+// Tests
+//----------------------------------------------------------------------
 describe("LanguageResolver", () => {
   let languageResolver: LanguageResolver;
-  let prisma: DeepMockProxy<PrismaClient>;
+  let languageService: DeepMockProxy<LanguageService>;
 
   beforeEach(async () => {
     const module = await Test.createTestingModule({
-      providers: [LanguageResolver, LanguageService, PrismaService],
+      providers: [LanguageResolver, LanguageService],
     })
-      .overrideProvider(PrismaService)
-      .useValue(mockDeep<PrismaClient>())
+      .overrideProvider(LanguageService)
+      .useValue(mockDeep<LanguageService>())
       .compile();
 
     languageResolver = module.get<LanguageResolver>(LanguageResolver);
-    prisma = module.get<PrismaService, DeepMockProxy<PrismaClient>>(PrismaService);
+    languageService = module.get<LanguageService, DeepMockProxy<LanguageService>>(LanguageService);
   });
 
-  it("should be defined", () => {
-    expect(languageResolver).toBeDefined();
+  it("create language", () => {
+    // ARRANGE
+    const createLanguageInput: CreateLanguageInput = { ...language };
+    languageService.create.mockResolvedValueOnce(language);
+
+    // ACT
+    const createdLanguage = languageResolver.createLanguage(createLanguageInput);
+
+    // ASSERT
+    expect(createdLanguage).resolves.toEqual(language);
+  });
+
+  it("get language", () => {
+    // ARRANGE
+    languageService.findOne.mockResolvedValueOnce(language);
+
+    // ACT
+    const foundLanguage = languageResolver.getLanguage(language.code);
+
+    // ASSERT
+    expect(foundLanguage).resolves.toEqual(language);
+  });
+
+  it("get languages", () => {
+    // ARRANGE
+    const languages = [language];
+    languageService.findAll.mockResolvedValueOnce(languages);
+
+    // ACT
+    const foundLanguages = languageResolver.getLanguages();
+
+    // ASSERT
+    expect(foundLanguages).resolves.toEqual(languages);
+  });
+
+  it("update language", () => {
+    // ARRANGE
+    const updateLanguageInput: UpdateLanguageInput = {
+      id: language.id,
+      code: "x",
+    };
+    languageService.update.mockResolvedValueOnce({ ...language, code: updateLanguageInput.code as string });
+
+    // ACT
+    const updatedLanguage = languageResolver.updateLanguage(updateLanguageInput);
+
+    // ASSERT
+    expect(updatedLanguage).resolves.toEqual({ ...language, code: updateLanguageInput.code });
+  });
+
+  it("remove language", () => {
+    // ARRANGE
+    const deletedAt = new Date();
+    languageService.remove.mockResolvedValueOnce({ ...language, deletedAt: deletedAt });
+
+    // ACT
+    const removedLanguage = languageResolver.removeLanguage(language.id);
+
+    // ASSERT
+    expect(removedLanguage).resolves.toEqual({ ...language, deletedAt: deletedAt });
   });
 });
