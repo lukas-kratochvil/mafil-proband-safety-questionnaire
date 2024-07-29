4d618a2f77b507c8b244c20fea6aa0ad86971121
Lukas Kratochvil <lkratochvil57@gmail.com>
2024-07-30 00:09:31 +0200

    feat: created tests for PDFResolver

--- backend/src/api/pdf/pdf.resolver.spec.ts
+++ backend/src/api/pdf/pdf.resolver.spec.ts
@@ -1,31 +1,67 @@
-import { ConfigService } from "@nestjs/config";
 import { Test } from "@nestjs/testing";
-import { PrismaClient } from "@prisma/client";
 import { DeepMockProxy, mockDeep } from "jest-mock-extended";
-import { EnvironmentVariables } from "@app/config";
-import { PrismaService } from "@app/prisma/prisma.service";
+import type { GeneratePDFArgs } from "./dto/generate-pdf.args";
+import type { PDFEntity } from "./entities/pdf.entity";
 import { PDFResolver } from "./pdf.resolver";
 import { PDFService } from "./pdf.service";
 
 describe("PDFResolver", () => {
   let pdfResolver: PDFResolver;
-  let config: ConfigService<EnvironmentVariables, true>;
-  let prisma: DeepMockProxy<PrismaClient>;
+  let pdfService: DeepMockProxy<PDFService>;
 
   beforeEach(async () => {
     const module = await Test.createTestingModule({
-      providers: [PDFResolver, PDFService, ConfigService, PrismaService],
+      providers: [PDFResolver, PDFService],
     })
-      .overrideProvider(PrismaService)
-      .useValue(mockDeep<PrismaClient>())
+      .overrideProvider(PDFService)
+      .useValue(mockDeep<PDFService>())
       .compile();
 
     pdfResolver = module.get<PDFResolver>(PDFResolver);
-    config = module.get<ConfigService<EnvironmentVariables, true>>(ConfigService);
-    prisma = module.get<PrismaService, DeepMockProxy<PrismaClient>>(PrismaService);
+    pdfService = module.get<PDFService, DeepMockProxy<PDFService>>(PDFService);
   });
 
-  it("should be defined", () => {
-    expect(pdfResolver).toBeDefined();
+  it("generate pdf", () => {
+    // ARRANGE
+    const args: GeneratePDFArgs = {
+      visitId: "VisitId",
+      isPhantom: false,
+      probandLanguageCode: "en",
+      projectAcronym: "ProjAcr",
+      measuredAt: new Date(),
+      genderCode: "M",
+      nativeLanguage: {
+        nativeName: "English",
+        nameCs: "Angličtina",
+      },
+      handednessCode: "RH",
+      finalizerUsername: "Operator",
+      name: "John",
+      surname: "Smith",
+      personalId: "123456",
+      birthdate: new Date(),
+      heightCm: 190,
+      weightKg: 85,
+      visualCorrectionDioptre: -1,
+      email: "john.smith@mail.com",
+      phone: "123456789",
+      answers: [
+        {
+          questionId: "Q1",
+          answer: "NO",
+        },
+      ],
+    };
+    const pdf: PDFEntity = {
+      name: "",
+      content: "",
+    };
+    pdfService.generate.mockResolvedValueOnce(pdf);
+
+    // ACT
+    const generatedPdf = pdfResolver.generatePDF(args);
+
+    // ASSERT
+    expect(generatedPdf).resolves.toEqual(pdf);
   });
 });
