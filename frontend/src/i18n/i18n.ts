746433cab76c3c6ac4f89e2808f6ae73b58d0504
Lukas Kratochvil <lkratochvil57@gmail.com>
2024-08-04 19:03:38 +0200

    fix: renamed back 'react-i18next.d.ts' and moved translations to a separate folder

--- frontend/src/i18n/i18n.ts
+++ frontend/src/i18n/i18n.ts
@@ -1,8 +1,8 @@
 import i18n from "i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
-import cs from "./cs.json";
-import en from "./en.json";
+import cs from "./translations/cs.json";
+import en from "./translations/en.json";
 
 export const convertStringToLocalizationKey = (str: string): TemplateStringsArray =>
   str as unknown as TemplateStringsArray;
