91ae026b2bb9af2fd425aeed9c96d22010ff36b0
Lukas Kratochvil <lkratochvil57@gmail.com>
2024-08-07 18:58:36 +0200

    feat: updated frontend dependency - @testing-library/jest-dom@6.4.8

--- frontend/src/tests/setup.ts
+++ frontend/src/tests/setup.ts
@@ -1,15 +1,7 @@
-import matchers, { type TestingLibraryMatchers } from "@testing-library/jest-dom/matchers";
+import "@testing-library/jest-dom/vitest";
 import { cleanup } from "@testing-library/react";
 import i18n from "@app/i18n/i18n";
 import { NEUTRAL_LANGUAGE_CODE } from "./neutral-language-code";
-
-// Extends Vitest's expect() method with methods from the react-testing-library
-declare module "vitest" {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  interface JestAssertion<T = any> extends jest.Matchers<void, T>, TestingLibraryMatchers<T, void> {}
-}
-
-expect.extend(matchers);
 
 // Set neutral language before each test case
 beforeEach(async () => {
