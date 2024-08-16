18cdcda8105c0d9c6161c84723e1ed87158ea192
Lukas Kratochvil <lkratochvil57@gmail.com>
2024-08-16 18:16:16 +0200

    feat: added error message for form RadioGroups

--- frontend/src/components/form/inputs/FormRadioGroup.tsx
+++ frontend/src/components/form/inputs/FormRadioGroup.tsx
@@ -1,39 +1,43 @@
 import { FormControlLabel, Radio, RadioGroup, type SxProps, type Theme } from "@mui/material";
 import { Controller } from "react-hook-form";
+import { ErrorMessage } from "./ErrorMessage";
 import type { FormDefaultInputProps } from "./input-props";
 
-type RadioProps<T> = {
+type RadioProps = {
   id: string;
   label: string;
-  value: T;
+  value: string;
 };
 
-type FormRadioGroupProps<T> = FormDefaultInputProps & {
-  radios: RadioProps<T>[];
-  defaultValue: T | null;
+type FormRadioGroupProps = FormDefaultInputProps & {
+  radios: RadioProps[];
+  defaultValue: RadioProps["value"] | null;
   sx?: SxProps<Theme>;
 };
 
-export const FormRadioGroup = <T,>({ name, disabled, radios, defaultValue, sx }: FormRadioGroupProps<T>) => (
-  <Controller
-    name={name}
-    // useForm() does not set default value for the radio group component, we need to set the defaultValue manually here due to: https://github.com/react-hook-form/react-hook-form/discussions/8153#discussioncomment-2533857
-    defaultValue={defaultValue}
-    render={({ field }) => (
-      <RadioGroup
-        {...field}
-        row
-        sx={sx}
-      >
-        {radios.map((radio) => (
-          <FormControlLabel
-            label={radio.label}
-            value={radio.value}
-            key={radio.id}
-            control={<Radio disabled={disabled} />}
-          />
-        ))}
-      </RadioGroup>
-    )}
-  />
+export const FormRadioGroup = ({ name, disabled, radios, defaultValue, sx }: FormRadioGroupProps) => (
+  <>
+    <Controller
+      name={name}
+      // useForm() does not set default value for the radio group component, we need to set the `defaultValue` manually here due to: https://github.com/react-hook-form/react-hook-form/discussions/8153#discussioncomment-2533857
+      defaultValue={defaultValue}
+      render={({ field }) => (
+        <RadioGroup
+          {...field}
+          row
+          sx={sx}
+        >
+          {radios.map((radio) => (
+            <FormControlLabel
+              label={radio.label}
+              value={radio.value}
+              key={radio.id}
+              control={<Radio disabled={disabled} />}
+            />
+          ))}
+        </RadioGroup>
+      )}
+    />
+    <ErrorMessage name={name} />
+  </>
 );
