287dcb444190e3cb2d7a0ac80f53ecd92f358bd5
Lukas Kratochvil <lkratochvil57@gmail.com>
2024-08-19 09:16:14 +0200

    feat: better NotFoundPage

--- frontend/src/pages/NotFoundPage.tsx
+++ frontend/src/pages/NotFoundPage.tsx
@@ -1,11 +1,21 @@
 import { Typography } from "@mui/material";
 import { useTranslation } from "react-i18next";
 import { useAuth } from "@app/hooks/auth/auth";
+import { PageContainer } from "./PageContainer";
 
 const NotFoundPage = () => {
   const { operator } = useAuth();
   const { t } = useTranslation("translation", { keyPrefix: "notFoundPage" });
-  return operator === undefined ? null : <Typography fontSize="2rem">{t("text")}</Typography>;
+
+  if (operator !== undefined) {
+    return null;
+  }
+
+  return (
+    <PageContainer center>
+      <Typography fontSize="2rem">{t("text")}</Typography>
+    </PageContainer>
+  );
 };
 
 export default NotFoundPage;
